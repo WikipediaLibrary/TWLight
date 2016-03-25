eca3c2a2da8c5f9f3c300a6d15c9ee3580cc6064
Andromeda Yelton <andromeda.yelton@gmail.com>
2016-03-25 12:16:28 -0400

    oh right, we need a mechanism for serving static in production

--- TWLight/wsgi.py
+++ TWLight/wsgi.py
@@ -14,3 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TWLight.settings.production")
 
 application = get_wsgi_application()
+
+try:
+    # If whitenoise is available, use it for serving staticfiles.
+    # If it's not, don't.
+    from whitenoise.django import DjangoWhiteNoise
+    application = DjangoWhiteNoise(application)
+except ImportError:
+    pass
