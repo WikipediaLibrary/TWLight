cc752a923058a5b93b7b7ab39e410be80de44484
Andromeda Yelton <andromeda.yelton@gmail.com>
2016-07-15 09:14:01 -0400

    Add coverage reporting to test runner

--- TWLight/settings/local.py
+++ TWLight/settings/local.py
@@ -17,3 +17,18 @@ EMAIL_HOST_USER = os.environ.get('DJANGO_EMAIL')
 EMAIL_HOST_PASSWORD = os.environ.get('DJANGO_EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_SENDING_OKAY = True
+
+# TEST CONFIGURATION
+# ------------------------------------------------------------------------------
+
+INSTALLED_APPS += (
+    'django_nose',
+)
+
+# Use nose to run all tests
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=TWLight.applications,TWLight.emails,TWLight.graphs,TWLight.resources, TWLight.users',
+]
