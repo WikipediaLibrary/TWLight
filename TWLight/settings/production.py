4557f0d61f36e7ac535c2b15e180c7ec2fbd7ea8
Andromeda Yelton <andromeda.yelton@gmail.com>
2016-07-27 13:33:15 +0000

    Supply local data

--- TWLight/settings/production.py
+++ TWLight/settings/production.py
@@ -19,5 +19,11 @@ SECRET_KEY = '8s8=)1direp%&imkq@91l)*9ot9^v*x+p@_6asq4z$k9kn&k*8'
 
 # Can be replaced with option files:
 # https://docs.djangoproject.com/en/1.7/ref/databases/#connecting-to-the-database
-DATABASES['default']['USER'] = 'TWLight'
-DATABASES['default']['PASSWORD'] = os.environ.get('DJANGO_MYSQL_PASSWORD', '')
+DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'twlight',
+        'USER': 'twlight',
+        'PASSWORD': 'Rf6@(y]@"&7;Dv]G',
+        'HOST': 'localhost',
+        'PORT': '3306',
+}
