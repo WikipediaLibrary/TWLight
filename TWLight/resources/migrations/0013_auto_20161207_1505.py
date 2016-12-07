7f69471231a670dbed780d6504b3d49face684dc
Andromeda Yelton <andromeda.yelton@gmail.com>
2016-12-07 10:10:08 -0500

    Add database migrations to support new status

---
+++ TWLight/resources/migrations/0013_auto_20161207_1505.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Migration #12 created the status field on Partner. It defaults to NOT_AVAILABLE,
+so as to avoid accidentally exposing Partners that aren't yet open for
+applications. However, all Partners existing in the database already need to be
+set as AVAILABLE.
+"""
+
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+def fix_partner_status(apps, schema_editor):
+    Partner = apps.get_model("resources", "Partner")
+    for partner in Partner.objects.all():
+        # This should be Partner.AVAILABLE. We can't reference that directly
+        # since it's not available to the migrations, though. Careful!
+        partner.status = 0
+        partner.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('resources', '0012_partner_status'),
+    ]
+
+    operations = [
+        migrations.RunPython(fix_partner_status),
+    ]
