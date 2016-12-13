8224b3af31596bc9e417cd231e5a3de969eb7f87
Andromeda Yelton <andromeda.yelton@gmail.com>
2016-12-13 13:17:01 -0500

    Fix autocompletion bugs
    1) PersonAutocomplete is inaccurate/misleading
    2) Autocomplete apparently uses the Django internal default queryset and
       not `objects`, so it was suggesting NOT_AVAILABLE Partners (which
       yielded DoesNotExist on form submission).

--- TWLight/resources/autocomplete_light_registry.py
+++ TWLight/resources/autocomplete_light_registry.py
@@ -2,8 +2,17 @@ import autocomplete_light
 
 from .models import Partner
 
-class PersonAutocomplete(autocomplete_light.AutocompleteModelBase):
+class PartnerAutocomplete(autocomplete_light.AutocompleteModelBase):
     search_fields = ['company_name']
     model = Partner
 
-autocomplete_light.register(PersonAutocomplete)
+    # Without this line, the autocomplete apparently supplies all Partners,
+    # even the NOT_AVAILABLE ones excluded by the default queryset, which causes
+    # form submission to fail with DoesNotExist when people filter for an
+    # unavailable partner. As this autocomplete is presented in a context where
+    # NOT_AVAILABLE partners aren't allowed, let's filter them out. If we need
+    # an autocomplete over all possible partners, create and register a
+    # separate one and use that in other contexts.
+    choices = Partner.objects.all()
+
+autocomplete_light.register(PartnerAutocomplete)
