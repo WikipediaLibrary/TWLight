abfce7293afa00f8d2bf561b86fd412b17db160c
Andromeda Yelton <andromeda.yelton@gmail.com>
2017-01-17 15:46:43 -0500

    Silence warnings produced by makemigrations

---
+++ TWLight/resources/migrations/0027_auto_20170117_2046.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('resources', '0026_partner_coordinator'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='partner',
+            name='languages',
+            field=models.ManyToManyField(help_text='Select all languages in which this partner publishes content.', to='resources.Language', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='stream',
+            name='languages',
+            field=models.ManyToManyField(to='resources.Language', blank=True),
+        ),
+    ]
