0d68c588e2f5fbf70737d335a07662d1a21d7ae1
Andromeda Yelton <andromeda.yelton@gmail.com>
2017-02-03 11:18:37 -0500

    Make tags translatable

--- TWLight/resources/translation.py
+++ TWLight/resources/translation.py
@@ -1,4 +1,6 @@
 from modeltranslation.translator import translator, TranslationOptions
+from taggit.models import Tag
+
 from .models import Partner
 
 # See https://django-modeltranslation.readthedocs.io/en/latest/registration.html
@@ -6,3 +8,10 @@ class PartnerTranslationOptions(TranslationOptions):
     fields = ('description',)
 
 translator.register(Partner, PartnerTranslationOptions)
+
+
+
+class TagTranslationTranslations(TranslationOptions):
+    fields = ('name',)
+
+translator.register(Tag, TagTranslationTranslations)
