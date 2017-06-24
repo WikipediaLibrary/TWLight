6fb60dfa1f3ce9cb7f5e33e2723f4277bed8dce8
Jason Sherman <jsn.sherman@gmail.com>
2017-06-24 18:01:09 -0500

    No need to reimplement a canned jijna filter provided by django.

--- TWLight/resources/templatetags/remove_tag.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def remove_tag(value, arg):
-  """Passes string through removetags filter."""
-  output = template.defaultfilters.removetags(value, arg)
-  return output
