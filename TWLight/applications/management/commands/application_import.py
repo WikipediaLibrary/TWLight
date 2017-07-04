dfc601ff14ed20f1cf554de4b59e23215e3feb8a
Jason Sherman <jsn.sherman@gmail.com>
2017-07-04 16:51:35 -0500

    1st cut of applications import command.

---
+++ TWLight/applications/management/commands/application_import.py
@@ -0,0 +1,54 @@
+import csv
+import logging
+
+from datetime import datetime
+from django.db import models
+from django.core.management.base import BaseCommand, CommandError
+from ....users.models import Editor
+from ....applications.models import Application
+from ....resources.models import Partner, Stream
+
+logger = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('file')
+
+    def handle(self, *args, **options):
+        with open(options['file']) as f:
+               reader = csv.reader(f)
+               # Skip first row, we expect it to be a header.
+               next(reader, None)  # skip the headers
+               for row in reader:
+                   try:
+                       # Where possible we're fetching the objects attaChed
+                       # to our attributes to verify they already exist.
+
+                       partner_id = row[0]
+                       partner = Partner.objects.get(pk=partner_id)
+
+                       # Inconsistent date format on the input files
+                       try:
+                           date_created = datetime.strptime(row[1], '%m/%d/%Y %H:%M').date()
+                       except:
+                           date_created = datetime.strptime(row[1], '%m/%d/%Y %H:%M:%S').date()
+
+                       wp_username=row[2]
+                       editor = Editor.objects.get(wp_username=wp_username)
+                       editor_id = editor.pk
+
+                       specific_stream_id = row[4]
+                       stream = Stream.objects.get(pk=specific_stream_id)
+                       Application.objects.get_or_create(
+                           partner_id = partner_id,
+                           date_created = date_created,
+                           date_closed = date_created,
+                           editor_id = editor_id,
+                           specific_stream_id = specific_stream_id,
+                           status = 4
+                       )
+                       logger.info("Application created.")
+                   except:
+                       logger.exception("Unable to create {wp_username}'s application to {partner_id}.".format(wp_username=row[2],partner_id=row[0]))
+                       pass
