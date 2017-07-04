7c532a7f6b57f6fde0d7e45ca6c5d86e40adf543
Jason Sherman <jsn.sherman@gmail.com>
2017-07-04 14:18:55 -0500

    Allow import file to be specified. Cleanup logging. Only hit API if editor doesn't already exist.

--- TWLight/users/management/commands/user_import.py
+++ TWLight/users/management/commands/user_import.py
@@ -7,70 +7,71 @@ from datetime import datetime
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from ....users.models import Editor,UserProfile
 
 logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('file')
+
     def handle(self, *args, **options):
-        # now do the things that you want with your models here
         user = models.OneToOneField(settings.AUTH_USER_MODEL)
-        with open('/vagrant/TaylorAndFrancisUserData.csv') as f:
+        with open(options['file']) as f:
                reader = csv.reader(f)
+               # Skip first row, we expect it to be a header.
+               next(reader, None)  # skip the headers
                for row in reader:
-                  if row[0] != '\xef\xbb\xbfPartner':
-                     logger.info('importing {row}.'.format(row=row))
-                     global_userinfo = self.get_global_userinfo_from_wp_username(row[2])
-                     if global_userinfo:
-                         # 2014-02-04T13:38:01Z
-                         logger.info('{info}.'.format(info=global_userinfo))
-                         reg_date = datetime.strptime(global_userinfo['registration'], '%Y-%m-%dT%H:%M:%SZ').date()
-                         try:
-                             logger.info("Found user.")
-                             username = global_userinfo['id']
-                             user = User.objects.get(username=username)
-                             created = False
-                         except User.DoesNotExist:
-                             logger.info("Can't find user; creating one.")
-                             user = User.objects.create_user(username=global_userinfo['id'], email=row[3])
-                             created = True
+                   wp_username = row[2]
+                   # We're wrapping this whole thing in a redundant-looking try
+                   # block to avoid hitting the API unnecessarily.
+                   try:
+                       editor = Editor.objects.get(wp_username=wp_username)
+                       logger.info("Editor exists.")
+                   except:
+                       global_userinfo = self.get_global_userinfo_from_wp_username(wp_username)
+                       if global_userinfo:
+                           logger.info('{info}.'.format(info=global_userinfo))
+                           reg_date = datetime.strptime(global_userinfo['registration'], '%Y-%m-%dT%H:%M:%SZ').date()
+                           try:
+                               username = global_userinfo['id']
+                               user = User.objects.get(username=username)
+                               created = False
+                               logger.info("User exists.")
+                           except User.DoesNotExist:
+                               logger.info("Can't find user; creating one.")
+                               user = User.objects.create_user(username=global_userinfo['id'], email=row[3])
+                               created = True
     
-                         if created:
-                             try:
-                                 Editor.objects.get_or_create(
-                                     user_id = user.pk,
-                                     wp_username = global_userinfo['name'],
-                                     wp_sub = global_userinfo['id'],
-                                     wp_editcount = global_userinfo['editcount'],
-                                     wp_registered = reg_date
-                                 )
-                                 logger.info("Can't find editor; creating one.")
-                             except:
-                                 logger.exception("Unable to create {editor}.".format(editor=global_userinfo['name']))
-                                 pass
+                           if created:
+                               try:
+                                   Editor.objects.get_or_create(
+                                       user_id = user.pk,
+                                       wp_username = global_userinfo['name'],
+                                       wp_sub = global_userinfo['id'],
+                                       wp_editcount = global_userinfo['editcount'],
+                                       wp_registered = reg_date
+                                   )
+                                   logger.info("Can't find editor; creating one.")
+                               except:
+                                   logger.exception("Unable to create {editor}.".format(editor=global_userinfo['name']))
+                                   pass
+                       pass
 
     def get_global_userinfo_from_wp_username(self,wp_username):
-        """
-        Grab global user information from the API, which we'll use to overlay
-        somme local wiki user info returned by OAuth.  Returns a dict like:
-
-        global_userinfo:
-          home:         "zhwikisource"
-          id:           27666025
-          registration: "2013-05-05T16:00:09Z"
-          name:         "Example"
-          editcount:    10
-        """
         try:
             endpoint = '{base}/w/api.php?action=query&meta=globaluserinfo&guiuser={name}&guiprop=editcount&format=json&formatversion=2'.format(base='https://meta.wikimedia.org',name=urllib2.quote(wp_username))
 
             results = json.loads(urllib2.urlopen(endpoint).read())
             global_userinfo = results['query']['globaluserinfo']
+            # If the user isn't found global_userinfo contains the empty key
+            # "missing"
             assert 'missing' not in global_userinfo
-            logger.info('user_import fetched global_userinfo for User {info}.'.format(info=global_userinfo))
+            logger.info('fetched global_userinfo for user')
             return global_userinfo
         except:
-            logger.exception('user_import could not fetch global_userinfo for {username}.'.format(username=urllib2.quote(wp_username)))
+            logger.exception('could not fetch global_userinfo for {username}.'.format(username=urllib2.quote(wp_username)))
             return None
             pass
