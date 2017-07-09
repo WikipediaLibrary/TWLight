4d0e58c854e6a0294cfdffc03ca52b705f295df8
Jason Sherman <jsn.sherman@gmail.com>
2017-07-09 14:16:07 -0500

    Add migrations

---
+++ TWLight/applications/migrations/0017_auto_20170709_1859.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('applications', '0016_auto_20170707_1552'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='application',
+            name='imported',
+            field=models.NullBooleanField(default=False),
+        ),
+        migrations.AlterField(
+            model_name='application',
+            name='date_created',
+            field=models.DateField(default=django.utils.timezone.now, editable=False),
+        ),
+    ]
