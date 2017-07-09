20ef537209da8c3f59346295e4872f1aaf538628
Jason Sherman <jsn.sherman@gmail.com>
2017-07-09 14:14:46 -0500

    wrap version_tags filters in try blocks.

--- TWLight/applications/templatetags/version_tags.py
+++ TWLight/applications/templatetags/version_tags.py
@@ -6,10 +6,16 @@ register = template.Library()
 
 @register.filter
 def get_status_display_for_version(version):
-    status = version.field_dict['status']
-    return Application.STATUS_CHOICES[status][1]
+    try:
+        status = version.field_dict['status']
+        return Application.STATUS_CHOICES[status][1]
+    except:
+        return None
 
 @register.filter
 def get_bootstrap_class_for_version(version):
-    status = version.field_dict['status']
-    return Application.LABELMAKER[status]
+    try:
+        status = version.field_dict['status']
+        return Application.LABELMAKER[status]
+    except:
+        return None
