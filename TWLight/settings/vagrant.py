edc79ce73f4cd6a80e4bba7020d975561f20f6aa
Jason Sherman <jsn.sherman@gmail.com>
2017-09-10 15:40:30 -0500

    Configure vagrant environment to run testing suite out of the box. https://phabricator.wikimedia.org/T175127

--- TWLight/settings/vagrant.py
+++ TWLight/settings/vagrant.py
@@ -58,3 +58,21 @@ CACHES = {
         'LOCATION': '/var/tmp/django_cache',
     }
 }
+
+# TEST CONFIGURATION
+# ------------------------------------------------------------------------------
+
+INSTALLED_APPS += (
+    'django_nose',
+)
+
+# Use nose to run all tests
+#TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = [
+    #'--with-coverage',
+    '--cover-package=TWLight.applications,TWLight.emails,TWLight.graphs,TWLight.resources, TWLight.users',
+    #'--nologcapture',
+    #'--cover-html',
+    #'--cover-html-dir=htmlcov',
+]
