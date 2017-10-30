6ee620d9078b7a64ca995727bf8e3dd2bbdb928e
Jason Sherman <jsn.sherman@gmail.com>
2017-10-30 04:44:47 -0500

    Capture migrations for non-functional user language pref field. https://phabricator.wikimedia.org/T178785

---
+++ TWLight/users/migrations/0024_userprofile_lang.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0023_auto_20170820_1623'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='userprofile',
+            name='lang',
+            field=models.CharField(blank=True, max_length=128, null=True, help_text='Language', choices=[(b'en', 'English'), (b'fi', 'Finnish'), (b'fr', 'French')]),
+        ),
+    ]
