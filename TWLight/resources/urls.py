e227f3af82fb38e31948177fcd61565a161d4150
Jason Sherman <jsn.sherman@gmail.com>
2018-03-23 11:03:45 +0000

    subtitute partner filter view for partner list view.

--- TWLight/resources/urls.py
+++ TWLight/resources/urls.py
@@ -6,9 +6,9 @@ from .filters import PartnerFilter
 from . import views
 
 urlpatterns = [
-    url(r'^$', 
-        views.PartnersListView.as_view(),
-        name='list'
+    url(r'^$',
+        views.PartnersFilterView.as_view(filterset_class=PartnerFilter),
+        name='filter'
     ),
     url(r'^(?P<pk>\d+)/$',
         views.PartnersDetailView.as_view(),
@@ -17,10 +17,5 @@ urlpatterns = [
     url(r'^toggle_waitlist/(?P<pk>\d+)/$',
         views.PartnersToggleWaitlistView.as_view(),
         name='toggle_waitlist'
-    ),
-    url(r'^filter/$',
-        views.PartnersFilterView.as_view(filterset_class=PartnerFilter),
-        #FilterView.as_view(filterset_class=PartnerFilter),
-        name='filter'
     ),
 ]
