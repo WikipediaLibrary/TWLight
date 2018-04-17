0c7c57fa8890fb8b570fe215f2b0c682adb8ca9a
Jason Sherman <jsn.sherman@gmail.com>
2018-04-17 15:56:59 +0000

    Upgrade to Django 1.10.

--- TWLight/i18n/urls.py
+++ TWLight/i18n/urls.py
@@ -1,10 +1,9 @@
 import warnings
 
 from django.conf import settings
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.core.urlresolvers import LocaleRegexURLResolver
 from django.utils import six
-from django.utils.deprecation import RemovedInDjango110Warning
 from TWLight.i18n.views import set_language
 
 # Direct rip from django.conf.urls.i18n, but imports our local set_language
@@ -17,13 +16,6 @@ def i18n_patterns(prefix, *args):
     URLconf.
     """
     if isinstance(prefix, six.string_types):
-        warnings.warn(
-            "Calling i18n_patterns() with the `prefix` argument and with tuples "
-            "instead of django.conf.urls.url() instances is deprecated and "
-            "will no longer work in Django 1.10. Use a list of "
-            "django.conf.urls.url() instances instead.",
-            RemovedInDjango110Warning, stacklevel=2
-        )
         pattern_list = patterns(prefix, *args)
     else:
         pattern_list = [prefix] + list(args)
