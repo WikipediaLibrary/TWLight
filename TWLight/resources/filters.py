70c0a4183fac3a67e25b659ba5b1bc3919cb7eba
Jason Sherman <jsn.sherman@gmail.com>
2018-04-18 13:49:53 +0000

    wrap django-filters that depend on django-taggit in a db check. The combination of taggit and filters caused a bootstrapping problem. This code depends on the django_content_type table which doesn't exist at migration time for new builds.

--- TWLight/resources/filters.py
+++ TWLight/resources/filters.py
@@ -1,10 +1,16 @@
 import django_filters
+from django.db import connection
+
 from taggit.managers import TaggableManager
 from .models import Language,Partner
 
-class PartnerFilter(django_filters.FilterSet):
-    tags = django_filters.ModelChoiceFilter(queryset=Partner.tags.all())
-    languages = django_filters.ModelChoiceFilter(queryset=Language.objects.all())
-    class Meta:
-        model = Partner
-        fields = ['languages', 'tags']
+class PartnerFilter(django_filters.FilterSet)
+    # The combination of taggit and filters caused a bootstrapping problem.
+    # This code depends on the django_content_type table which doesn't exist at
+    # migration time for new builds, so it's wrapped in a check for the table.
+    if 'django_content_type' in connection.introspection.table_names():
+        tags = django_filters.ModelChoiceFilter(queryset=Partner.tags.all())
+        languages = django_filters.ModelChoiceFilter(queryset=Language.objects.all())
+        class Meta:
+            model = Partner
+            fields = ['languages', 'tags']
