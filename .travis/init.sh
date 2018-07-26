72d89801f34caf550f4c49b8217e77b70140366f
Jason Sherman <jsn.sherman@gmail.com>
2018-07-26 23:13:03 +0000

    Capture work to include translation in travis execution.

--- .travis/init.sh
+++ .travis/init.sh
@@ -6,7 +6,15 @@ mysql < .travis/init_db.sql
 # Write config file.
 cp .travis/local_vars.py TWLight/settings/local_vars.py
 
-# Initialize Django app: make migrations, migrate, sync translations, collect static.
+# Initialize Django app:
+# make migrations
 python manage.py makemigrations
+
+# Migrate and sync translation fields.
 python manage.py migrate || bash -c "python manage.py sync_translation_fields --noinput && python manage.py migrate"
+
+# Compile translations. Slightly involved due to this being a Wikimedia project.
+.travis/./translate.sh
+
+# Collect static assets
 python manage.py collectstatic --noinput --clear
