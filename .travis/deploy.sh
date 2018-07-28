ff69ff4d293c11259584f8392c61425075283f66
Jason Sherman <jsn.sherman@gmail.com>
2018-07-28 18:40:29 +0000

    Try to keep production fully synced to master.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -26,8 +26,8 @@ then
         git remote rm origin
         git remote add origin https://${gh_bot_username}:${gh_bot_token}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
         git branch --quiet --set-upstream-to origin/production
-        git pull --quiet
-        git push --quiet
+        git pull origin production --quiet
+        git push origin production --quiet
         echo "Build pushed to production."
     }
 
