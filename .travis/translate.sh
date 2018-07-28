3b636d20d58c2f718338d5cddf6ded25b0f10d17
Jason Sherman <jsn.sherman@gmail.com>
2018-07-28 18:03:25 +0000

    Only fire translations in travis if the previous commit changed files that get searched by makemessages. Also, update translations.

--- .travis/translate.sh
+++ .travis/translate.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
-# Compile translations
-echo "makemessages"
-langs=($(python manage.py diffsettings | grep '^LANGUAGES =' | grep -o "(u'[^']*'" | grep -o "'[^']*'"  | xargs))
-for locale in "${langs[@]}"; do
-  python manage.py makemessages --locale=${locale} || exit 1
-done
-python manage.py makemessages --locale=qqq || exit 1
+# Count the number of files searched by makemessages
+# that were changed in the last commit.
+# https://docs.djangoproject.com/en/1.11/ref/django-admin/
+message_files_changed=$(git diff --name-only HEAD~1..HEAD -- '*.html' '*.txt' '*.py' | wc -l)
 
-echo "compilemessages"
-python manage.py compilemessages || exit 1
+# If any relevant files changed, update translations.
+if [ "${message_files_changed}" -gt 0 ]
+then
+    # Compile translations
+    echo "makemessages"
+    langs=($(python manage.py diffsettings | grep '^LANGUAGES =' | grep -o "(u'[^']*'" | grep -o "'[^']*'"  | xargs))
+    for locale in "${langs[@]}"; do
+      python manage.py makemessages --locale=${locale} || exit 1
+    done
+    python manage.py makemessages --locale=qqq || exit 1
+    
+    echo "compilemessages"
+    python manage.py compilemessages || exit 1
+fi
