2d5ab50217a1d57a50749cbe6133e11b33f03458
Jason Sherman <jsn.sherman@gmail.com>
2018-07-28 18:07:53 +0000

    Don't run translations in travis if the commit containing the changed message files also contains changed translation files

--- .travis/translate.sh
+++ .travis/translate.sh
@@ -4,9 +4,11 @@
 # that were changed in the last commit.
 # https://docs.djangoproject.com/en/1.11/ref/django-admin/
 message_files_changed=$(git diff --name-only HEAD~1..HEAD -- '*.html' '*.txt' '*.py' | wc -l)
+translation_files_changed=$(git diff --name-only HEAD~1..HEAD -- '*.po' | wc -l)
 
-# If any relevant files changed, update translations.
-if [ "${message_files_changed}" -gt 0 ]
+# If any relevant files changed but no translation files changed,
+# update translations.
+if [ "${message_files_changed}" -gt 0 ] && [ "${translation_files_changed}" -eq 0 ]
 then
     # Compile translations
     echo "makemessages"
