8e8fe41b624040a533a6cf11639528d163a8a472
Jason Sherman <jsn.sherman@gmail.com>
2018-08-01 13:59:56 +0000

    Change ops execution order to line up with travis.

--- bin/virtualenv_update.sh
+++ bin/virtualenv_update.sh
@@ -14,11 +14,11 @@ git pull origin ${TWLIGHT_GIT_REVISION}
 # Update pip dependencies.
 bash ${TWLIGHT_HOME}/bin/virtualenv_pip_update.sh || exit 1
 
+# Process static assets.
+bash ${TWLIGHT_HOME}/bin/virtualenv_clearstatic.sh || exit 1
+
 # Run migrations.
 bash ${TWLIGHT_HOME}/bin/virtualenv_migrate.sh || exit 1
 
 # Compile translations.
 bash ${TWLIGHT_HOME}/bin/virtualenv_translate.sh || exit 1
-
-# Process static assets.
-bash ${TWLIGHT_HOME}/bin/virtualenv_clearstatic.sh || exit 1
