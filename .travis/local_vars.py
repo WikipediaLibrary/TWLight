70d0c251957d495f039f8553e2037cdc8e3f6c9f
Jason Sherman <jsnshrmn@users.noreply.github.com>
2018-12-04 07:57:24 -0600

    Travis no longer needs its own Matomo config.

--- .travis/local_vars.py
+++ .travis/local_vars.py
@@ -5,8 +5,6 @@ TWLIGHT_OAUTH_CONSUMER_KEY='null'
 TWLIGHT_OAUTH_CONSUMER_SECRET='null'
 ALLOWED_HOSTS = ['localhost']
 REQUEST_BASE_URL = 'http://localhost/'
-MATOMO_HOSTNAME = None
-MATOMO_SITE_ID = None
 # Ugh. Have to set debug to false to get the storage engine to return URLS with hashes.
 # Travis was returning some interesting errors when this was not happening.
 # https://docs.djangoproject.com/en/1.11/ref/contrib/staticfiles/#manifeststaticfilesstorage
