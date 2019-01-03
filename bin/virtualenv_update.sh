78e2eb0b3a9db4fa03cc2829d110c2f6f4865da8
Jason Sherman <jsn.sherman@gmail.com>
2019-01-03 17:37:18 +0000

    prepare bin/virtualenv_update.sh to be run as root.

--- bin/virtualenv_update.sh
+++ bin/virtualenv_update.sh
@@ -1,38 +1,40 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+if [ "${USER}" != "root" ]
+then
+    echo "twlight_update.sh must be run as root!"
+    exit 1
+fi
+
 # Environment variables may not be loaded under all conditions.
 if [ -z "${TWLIGHT_HOME}" ]
 then
     source /etc/environment
 fi
 
-# Verify that the unix user has proper group membership.
-if groups | grep &>/dev/null "\b${TWLIGHT_UNIXNAME}\b"
+# Verify that the unix user can talk to the remote.
+cd ${TWLIGHT_HOME}
+if git fetch --dry-run
 then
-    # Verify that the unix user can talk to the remote.
-    cd ${TWLIGHT_HOME}
-    if git fetch --dry-run
-    then
-        # Fetch latest code from ${TWLIGHT_GIT_REVISION}.
-        git checkout .
-        git pull origin ${TWLIGHT_GIT_REVISION}
-    else
-        exit 1
-    fi
+    # Fetch latest code from ${TWLIGHT_GIT_REVISION}.
+    git checkout .
+    git pull origin ${TWLIGHT_GIT_REVISION}
+
+    # Make sure ${TWLIGHT_UNIXNAME} owns the .git tree
+    chown -R ${TWLIGHT_UNIXNAME}:${TWLIGHT_UNIXNAME} ${TWLIGHT_HOME}/.git
 else
-    echo "git pull must be run as member of ${TWLIGHT_UNIXNAME} group!"
     exit 1
 fi
 
 # Update pip dependencies.
-bash ${TWLIGHT_HOME}/bin/virtualenv_pip_update.sh
+sudo su ${TWLIGHT_UNIXNAME} bash ${TWLIGHT_HOME}/bin/virtualenv_pip_update.sh
 
 # Process static assets.
-bash ${TWLIGHT_HOME}/bin/virtualenv_clearstatic.sh
+sudo su ${TWLIGHT_UNIXNAME} bash ${TWLIGHT_HOME}/bin/virtualenv_clearstatic.sh
 
 # Run migrations.
-bash ${TWLIGHT_HOME}/bin/virtualenv_migrate.sh
+sudo su ${TWLIGHT_UNIXNAME} bash ${TWLIGHT_HOME}/bin/virtualenv_migrate.sh
 
 # Compile translations.
-bash ${TWLIGHT_HOME}/bin/virtualenv_translate.sh
+sudo su ${TWLIGHT_UNIXNAME} bash ${TWLIGHT_HOME}/bin/virtualenv_translate.sh
