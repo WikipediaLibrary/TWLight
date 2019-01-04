6a542b0ab96d24e3201bb69507086044c98a6793
Jason Sherman <jsn.sherman@gmail.com>
2019-01-04 08:00:00 -0600

    Drain git logic from twlight_update_code.sh into twlight_update.sh

--- bin/twlight_update.sh
+++ bin/twlight_update.sh
@@ -8,6 +8,7 @@ then
 fi
 
 {
+
     # print the date for logging purposes
     echo [$(date)]
 
@@ -18,29 +19,75 @@ fi
         exit 1
     fi
 
-    # Verify that the unix user can talk to the remote.
-    cd ${TWLIGHT_HOME}
-    if git fetch --dry-run
+    # init is basically a --force flag.
+    if [ "${1}" = "init" ]
     then
+         init="true"
+    fi
+
+    git_check() {
+        # Verify that the unix user can talk to the remote.
+        cd ${TWLIGHT_HOME}
+        if git fetch --dry-run
+        then
+
+        # Check the remote status.
+        git fetch
+
+        # Only do stuff if we're behind the remote. Cribbed from stackoverflow:
+        # https://stackoverflow.com/a/3278427
+        upstream=${1:-'@{u}'}
+        local=$(git rev-parse @)
+        remote=$(git rev-parse "$upstream")
+        base=$(git merge-base @ "$upstream")
+
+        if [ "${local}" = "${remote}" ]
+        then
+            echo "Up-to-date"
+            exit 0
+        elif [ "${local}" = "${base}" ]
+        then
+            echo "Need to pull"
+            return 0
+        elif [ "${remote}" = "${base}" ]
+        then
+            echo "Need to push"
+            return 1
+        else
+            echo "Diverged"
+            return 1
+        fi
+    }
+
+    git_pull() {
         # Fetch latest code from ${TWLIGHT_GIT_REVISION}.
-        git checkout .
-        git pull origin ${TWLIGHT_GIT_REVISION}
+        cd ${TWLIGHT_HOME}
+        git pull ${TWLIGHT_GIT_REVISION}
 
         # Make sure ${TWLIGHT_UNIXNAME} owns everything.
         chown -R ${TWLIGHT_UNIXNAME}:${TWLIGHT_UNIXNAME} ${TWLIGHT_HOME}
+    }
+
+    virtualenv_update() {
+        # Update pip dependencies.
+        sudo su ${TWLIGHT_UNIXNAME} ${TWLIGHT_HOME}/bin/virtualenv_pip_update.sh
+
+        # Process static assets.
+        sudo su ${TWLIGHT_UNIXNAME} ${TWLIGHT_HOME}/bin/virtualenv_clearstatic.sh
+
+        # Run migrations.
+        sudo su ${TWLIGHT_UNIXNAME} ${TWLIGHT_HOME}/bin/virtualenv_migrate.sh
+
+        # Compile translations.
+        sudo su ${TWLIGHT_UNIXNAME} ${TWLIGHT_HOME}/bin/virtualenv_translate.sh
+    }
+
+    if git_check
+    then
+       git_pull
+       virtualenv_update
     else
         exit 1
     fi
 
-    # Update pip dependencies.
-    sudo su ${TWLIGHT_UNIXNAME} ${TWLIGHT_HOME}/bin/virtualenv_pip_update.sh
-
-    # Process static assets.
-    sudo su ${TWLIGHT_UNIXNAME} ${TWLIGHT_HOME}/bin/virtualenv_clearstatic.sh
-
-    # Run migrations.
-    sudo su ${TWLIGHT_UNIXNAME} ${TWLIGHT_HOME}/bin/virtualenv_migrate.sh
-
-    # Compile translations.
-    sudo su ${TWLIGHT_UNIXNAME} ${TWLIGHT_HOME}/bin/virtualenv_translate.sh
 } 2>&1 | tee -a ${TWLIGHT_HOME}/TWLight/logs/update.log
--- bin/twlight_update_code.sh
+++ bin/twlight_update_code.sh
@@ -6,62 +6,30 @@ then
     source /etc/environment
 fi
 
+cd ${TWLIGHT_HOME}
+
 if [ "${1}" = "init" ]
 then
-     init="true"
+    venv_update_cmd="${TWLIGHT_HOME}/bin/./twlight_update.sh"
+else
+    venv_update_cmd="${TWLIGHT_HOME}/bin/./twlight_update.sh init"
 fi
 
-cd ${TWLIGHT_HOME}
-update() {
-    venv_update_cmd="${TWLIGHT_HOME}/bin/./twlight_update.sh"
-    venv_test_cmd="${TWLIGHT_HOME}/bin/./virtualenv_test.sh"
+venv_test_cmd="${TWLIGHT_HOME}/bin/./virtualenv_test.sh"
 
-    # Make sure ${TWLIGHT_UNIXNAME} owns the .git tree
-    chown -R ${TWLIGHT_UNIXNAME}:${TWLIGHT_UNIXNAME} ${TWLIGHT_HOME}/.git
+# Make sure ${TWLIGHT_UNIXNAME} owns the .git tree
+chown -R ${TWLIGHT_UNIXNAME}:${TWLIGHT_UNIXNAME} ${TWLIGHT_HOME}/.git
 
-    # Pull latest git, apply changes to Django
-    # If that succeeds, run the test suite
-    # If that succeeds, restart Green Unicorn
+# Pull latest git, apply changes to Django
+# If that succeeds, run the test suite
+# If that succeeds, restart Green Unicorn
 
-    if sudo bash -c "${venv_update_cmd}"; then
-        if sudo su ${TWLIGHT_UNIXNAME} bash -c "${venv_test_cmd}"; then
-            systemctl restart gunicorn || exit 1
-        else
-            exit 1
-        fi
+if sudo bash -c "${venv_update_cmd}"; then
+    if sudo su ${TWLIGHT_UNIXNAME} bash -c "${venv_test_cmd}"; then
+        systemctl restart gunicorn || exit 1
     else
         exit 1
     fi
-}
-
-if [ "${init}" = "true" ]
-then
-    echo "Init"
-    update
 else
-    # Check the remote status.
-    git fetch
-
-    # Only do stuff if we're behind the remote. Cribbed from stackoverflow:
-    # https://stackoverflow.com/a/3278427
-    upstream=${1:-'@{u}'}
-    local=$(git rev-parse @)
-    remote=$(git rev-parse "$upstream")
-    base=$(git merge-base @ "$upstream")
-
-    if [ "${local}" = "${remote}" ]
-    then
-        echo "Up-to-date"
-    elif [ "${local}" = "${base}" ]
-    then
-        echo "Need to pull"
-        update
-    elif [ "${remote}" = "${base}" ]
-    then
-        echo "Need to push"
-        exit 1
-    else
-        echo "Diverged"
-        exit 1
-    fi
+    exit 1
 fi
