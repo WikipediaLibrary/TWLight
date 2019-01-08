b10a0aefc72769a4dcf827daaf766e5f84ce4686
Jason Sherman <jsnshrmn@users.noreply.github.com>
2019-01-08 09:15:58 -0600

    set email from address in twlight_deploy.sh

--- bin/twlight_deploy.sh
+++ bin/twlight_deploy.sh
@@ -10,5 +10,5 @@ update="${TWLIGHT_HOME}/bin/./twlight_update_code.sh"
 
 if failure=$( ! ${update} ) && [ -n "${TWLIGHT_ERROR_MAILTO+isset}" ]
 then
-    echo ${failure} | mail -s "${update} failed for $(hostname -f)" ${TWLIGHT_ERROR_MAILTO}
+    echo ${failure} | mail -a "From: Wikipedia Library Card Platform <noreply@wikipedialibrary.wmflabs.org>" -s "${update} failed for $(hostname -f)" ${TWLIGHT_ERROR_MAILTO}
 fi
