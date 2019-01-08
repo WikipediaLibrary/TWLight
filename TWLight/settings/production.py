6900d2fb97ae88fc5c9167082622ffa3d35d8528
Jason Sherman <jsnshrmn@users.noreply.github.com>
2019-01-08 09:12:54 -0600

    hotfix wmf email infrastructure change.

--- TWLight/settings/production.py
+++ TWLight/settings/production.py
@@ -51,7 +51,7 @@ DATABASES['default']['PASSWORD'] = MYSQL_PASSWORD
 DJMAIL_REAL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_BACKEND = 'djmail.backends.async.EmailBackend'
-EMAIL_HOST = 'tools-mail.tools.eqiad.wmflabs'
+EMAIL_HOST = 'mx-out01.wmflabs.org'
 EMAIL_PORT = 25
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
