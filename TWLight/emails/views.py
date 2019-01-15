97b126dbe3ba8f457312f1c8743abf3b778b760c
Aaron Vasanth <uyscuti.wiki@gmail.com>
2019-01-15 06:02:38 +0000

    new views.py

---
+++ TWLight/emails/views.py
@@ -0,0 +1,56 @@
+from django.contrib import messages
+
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.core.mail import BadHeaderError, send_mail
+from django.http import HttpResponse, HttpResponseRedirect
+from django.utils.decorators import method_decorator
+from django.utils.translation import ugettext_lazy as _
+from django.views.generic.edit import FormView
+
+from TWLight.emails.forms import ContactUsForm
+
+@method_decorator(login_required, name='post')
+class ContactUsView(FormView):
+    template_name = 'emails/contact.html'
+    form_class = ContactUsForm
+    success_url = reverse_lazy('contact')
+
+    def get_initial(self):
+        initial = super(ContactUsView, self).get_initial()
+        # @TODO: This sort of gets repeated in SuggestionForm.
+        # We could probably be factored out to a common place for DRYness.
+        if ('email' in self.request.GET):
+            initial.update({
+                 'email': self.request.GET['email'],
+            })
+        if ('message' in self.request.GET):
+            initial.update({
+                 'message': self.request.GET['message'],
+            })
+        initial.update({
+            'next': reverse_lazy('contact'),
+        })
+
+        return initial
+
+    def form_valid(self, form):
+        # Adding an extra check to ensure the user is a wikipedia editor.
+        try:
+            assert self.request.user.editor
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail('Aww', message, email, ['aaronvasanth66@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            messages.add_message(self.request, messages.SUCCESS,
+            # Translators: Shown to users when they successfully add a new partner suggestion.
+            _('Your message has been sent. We''ll get back to you soon!'))
+            return HttpResponseRedirect(reverse('contact'))
+        except (AssertionError, AttributeError) as e:
+            messages.add_message (self.request, messages.WARNING,
+                # Translators: This message is shown to non-wikipedia editors who attempt to post data to suggestion form.
+                _('You must be a Wikipedia editor to do that.'))
+            raise PermissionDenied
+        return self.request.user.editor
