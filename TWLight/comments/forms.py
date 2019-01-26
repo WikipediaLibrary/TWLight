868ecbd7f8858e2838b8c33b3185f30de8ac30ef
Aaron Vasanth <uyscuti.wiki@gmail.com>
2019-01-26 09:32:46 +0000

    method overide with two popped fields

---
+++ TWLight/comments/forms.py
@@ -0,0 +1,24 @@
+import datetime
+
+from django_comments.forms import CommentForm
+from django.contrib.contenttypes.models import ContentType
+from django.utils.encoding import force_unicode
+from django.conf import settings
+
+
+class CommentWithoutEmail(CommentForm):
+    def get_comment_create_data(self):
+        # Overide the default comment form behaviour, with two left out fields
+        return dict(
+            content_type = ContentType.objects.get_for_model(self.target_object),
+            object_pk    = force_unicode(self.target_object._get_pk_val()),
+            user_name    = self.cleaned_data["name"],
+            comment      = self.cleaned_data["comment"],
+            submit_date  = datetime.datetime.now(),
+            site_id      = settings.SITE_ID,
+            is_public    = True,
+            is_removed   = False,
+        )
+
+CommentWithoutEmail.base_fields.pop('url')
+CommentWithoutEmail.base_fields.pop('email')
