c135d4b1d688ae94631c5db358277b2477c5c090
Aaron Vasanth Joseph <uyscuti.wiki@gmail.com>
2019-02-09 18:52:13 +0000

    add celery to take care of async

--- TWLight/settings/base.py
+++ TWLight/settings/base.py
@@ -358,3 +358,10 @@ MIDDLEWARE_CLASSES += ['request.middleware.RequestMiddleware',]
 # as tracking only authenticated vs anonymous users).
 REQUEST_LOG_IP = False
 REQUEST_LOG_USER = False
+
+# CELERY CONFIGURATION
+# ------------------------------------------------------------------------------
+BROKER_URL = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
