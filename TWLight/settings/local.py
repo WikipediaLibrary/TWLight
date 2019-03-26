11b929493fd028d84de466edd61410ba89fa107d
Jason Sherman <jsn.sherman@gmail.com>
2019-03-26 12:09:07 -0500

    move all local vars to environment variables that can be managed by docker.

--- TWLight/settings/local.py
+++ TWLight/settings/local.py
@@ -12,26 +12,16 @@ from __future__ import print_function
 import sys
 
 from .base import *
-try:
-    from .local_vars import (SECRET_KEY,
-                                  TWLIGHT_OAUTH_PROVIDER_URL,
-                                  TWLIGHT_OAUTH_CONSUMER_KEY,
-                                  TWLIGHT_OAUTH_CONSUMER_SECRET,
-                                  MYSQL_PASSWORD,
-                                  ALLOWED_HOSTS,
-                                  DEBUG,
-                                  REQUEST_BASE_URL)
-except ImportError:
-    # If there's no local_vars file on this system (e.g. because it isn't
-    # a local system), this import will fail, which can cause things
-    # to fail (notably makemigrations).
-    print('Cannot import from TWLight/settings/local_vars.py',
-          'This is fine if you are not on a local system, as long as your '
-          'settings file is something other than TWLight/settings/local, '
-          'but it will cause the app to fail if you are trying to use '
-          'local settings.',
-          file=sys.stderr)
-    raise
+
+SECRET_KEY = os.environ.get('SECRET_KEY', None)
+TWLIGHT_OAUTH_PROVIDER_URL = os.environ.get('TWLIGHT_OAUTH_PROVIDER_URL', None)
+TWLIGHT_OAUTH_CONSUMER_KEY = os.environ.get('TWLIGHT_OAUTH_CONSUMER_KEY', None)
+TWLIGHT_OAUTH_CONSUMER_SECRET = os.environ.get('TWLIGHT_OAUTH_CONSUMER_SECRET', None)
+MYSQL_USER = os.environ.get('MYSQL_USER', None)
+MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', None)
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', None).split(' ')
+DEBUG = os.environ.get('DEBUG', False)
+REQUEST_BASE_URL = os.environ.get('REQUEST_BASE_URL', None)
 
 # Let Django know about external URLs in case they differ from internal
 # Needed to be added for /admin
@@ -39,7 +29,7 @@ USE_X_FORWARDED_HOST = True
 
 # Can be replaced with option files:
 # https://docs.djangoproject.com/en/1.7/ref/databases/#connecting-to-the-database
-DATABASES['default']['USER'] = 'twlight'
+DATABASES['default']['USER'] = MYSQL_USER
 DATABASES['default']['PASSWORD'] = MYSQL_PASSWORD
 
 DEFAULT_FROM_EMAIL = '<twlight.local@localhost.localdomain>'
