3fe66f5f1650c7e7e50af4e080e7657b8e0b1f5f
Jason Sherman <jsn.sherman@gmail.com>
2019-04-04 20:09:08 -0500

    make root pw optional in db init script.

--- bin/twlight_mysql_init.sh
+++ bin/twlight_mysql_init.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-mysql -u root -p${MYSQL_ROOT_PASSWORD} <<EOF
+if  [ -n "${MYSQL_ROOT_PASSWORD+isset}" ]
+then
+    mysql_cmd="mysql -u root -p${MYSQL_ROOT_PASSWORD}"
+else
+    mysql_cmd="mysql -u root"
+fi
+
+${mysql_cmd} <<EOF
 CREATE DATABASE IF NOT EXISTS ${DJANGO_DB_NAME};
 GRANT ALL PRIVILEGES on ${DJANGO_DB_NAME}.* to ${DJANGO_DB_USER}@'%' IDENTIFIED BY '${DJANGO_DB_PASSWORD}';
 GRANT ALL PRIVILEGES on test_${DJANGO_DB_NAME}.* to ${DJANGO_DB_USER}@'%' IDENTIFIED BY '${DJANGO_DB_PASSWORD}';
