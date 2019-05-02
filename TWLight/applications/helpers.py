9ebfba469cb34825badfaa8163e1a66a2f6c1d2f
uyscuti-wiki <uyscuti.wiki@gmail.com>
2019-05-02 09:53:51 +0000

    unwaitlist mgmt. cmd.

--- TWLight/applications/helpers.py
+++ TWLight/applications/helpers.py
@@ -158,7 +158,7 @@ def get_output_for_application(app):
 
 
 def get_active_authorizations(partner_pk, stream_pk=None):
-    today= datetime.date.today()
+    today = datetime.date.today()
     filter_dict = {'date_expires__gt': today, 'partner': partner_pk} if stream_pk is None else {'date_expires__gt': today, 'partner': partner_pk, 'stream': stream_pk}
     try:
         active_authorizations = Authorization.objects.filter(**filter_dict).count()
