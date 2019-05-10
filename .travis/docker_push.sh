6f04b7d56762abbe546ade3e8e20a85487a978e3
Jason Sherman <jsn.sherman@gmail.com>
2019-05-10 08:17:26 -0500

    1st pass of travis building and pushing docker images.

---
+++ .travis/docker_push.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Only act if this is build is happening directly on the docker branch.
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ -z "${TRAVIS_TAG}" ] && [ "${TRAVIS_BRANCH}" = "docker" ]
+then
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+  docker push wikipedialibrary/twlight:latest
+fi
