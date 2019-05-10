40875fb6e5e764868f4cc463c053314aaf544d9d
Jason Sherman <jsn.sherman@gmail.com>
2019-05-10 08:54:08 -0500

    adjust docker image tags

--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# Only act if this is build is happening directly on the docker branch.
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ -z "${TRAVIS_TAG}" ] && [ "${TRAVIS_BRANCH}" = "docker" ]
+# Only act if this is build is happening directly on a non-default branch.
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ -z "${TRAVIS_TAG}" ] && [ "${TRAVIS_BRANCH}" != "master" ]
 then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-  docker push wikipedialibrary/twlight:latest
+  docker push wikipedialibrary/twlight:${TRAVIS_BRANCH}
 fi
