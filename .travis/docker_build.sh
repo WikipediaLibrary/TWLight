a78bb22c7990a87d66adb07ac5ed55e86c9a8e07
Jason Sherman <jsn.sherman@gmail.com>
2019-05-10 09:08:18 -0500

    make sure travis builds the correct docker tag

--- .travis/docker_build.sh
+++ .travis/docker_build.sh
@@ -4,5 +4,5 @@
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ -z "${TRAVIS_TAG}" ] && [ "${TRAVIS_BRANCH}" = "docker" ]
 then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-  docker build -t wikipedialibrary/twlight:latest .
+  docker build -t wikipedialibrary/twlight:${TRAVIS_BRANCH} .
 fi
