6742343bb3f1b6fcbbcad0fd3142c8964b898e8f
Jason Sherman <jsn.sherman@gmail.com>
2019-05-11 13:32:13 -0500

    login to dockerhub before trying to pull an existing image to warm the travis build cache.

--- .travis/docker_pull.sh
+++ .travis/docker_pull.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker pull wikipedialibrary/twlight:${TRAVIS_BRANCH} || :
+docker pull wikipedialibrary/twlight:${TRAVIS_BRANCH} || true
