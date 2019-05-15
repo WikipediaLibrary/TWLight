fd1f561f40c76d38a4f4327aa002c52307f627a8
uyscuti-wiki <uyscuti.wiki@gmail.com>
2019-05-15 14:07:25 +0000

    clarify

--- bin/twlight_unwaitlist.sh
+++ bin/twlight_unwaitlist.sh
@@ -11,7 +11,7 @@ fi
 # Load virtual environment
 if source ${TWLIGHT_HOME}/bin/virtualenv_activate.sh
 then
-    echo "Probing for waitlisted partners with atleast one active authorization"
+    echo "Probing for waitlisted partners with atleast one available account"
     python manage.py proxy_waitlist_disable || exit
 else
     exit 1
