62a57844d7be83f82161d0be818208f5d892c695
Jason Sherman <jsn.sherman@gmail.com>
2019-06-10 07:14:17 -0500

    take a stab at using build images to cache travis processes.

---
+++ .travis/docker_compose.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+docker build --cache-from "wikipedialibrary/twlight_base:${TRAVIS_BRANCH}" --target twlight_base --tag "twlight_base" .
+docker build --cache-from "wikipedialibrary/twlight_build:${TRAVIS_BRANCH}" --target twlight_build --tag "twlight_build" .
+docker build --cache-from "twlight_base" --target twlight_base --tag "wikipedialibrary/twlight_base:${TRAVIS_BRANCH}" .
+docker build --cache-from "twlight_build" --target twlight_build --tag "wikipedialibrary/twlight_build:${TRAVIS_BRANCH}" .
+docker-compose -f docker-compose.yml -f docker-compose.travis.yml up -d db twlight
--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -4,5 +4,7 @@
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ -z "${TRAVIS_TAG}" ] && [ "${TRAVIS_BRANCH}" != "master" ]
 then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+  docker push wikipedialibrary/twlight_base:${TRAVIS_BRANCH}
+  docker push wikipedialibrary/twlight_build:${TRAVIS_BRANCH}
   docker push wikipedialibrary/twlight:${TRAVIS_BRANCH}
 fi
