066eefa601250be0bab3dc54d12416fdd39294b1
Jason Sherman <jsn.sherman@gmail.com>
2019-06-10 15:50:33 -0500

    try to fix docker compose permissions issue in travis.

--- .travis/docker_compose.sh
+++ .travis/docker_compose.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
+set -euxo pipefail
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build --cache-from "wikipedialibrary/twlight_base:${TRAVIS_BRANCH}" --target twlight_base --tag "twlight_base" .
 docker build --cache-from "wikipedialibrary/twlight_build:${TRAVIS_BRANCH}" --target twlight_build --tag "twlight_build" .
 docker build --cache-from "twlight_base" --target twlight_base --tag "wikipedialibrary/twlight_base:${TRAVIS_BRANCH}" .
