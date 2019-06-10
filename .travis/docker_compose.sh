7dffc596099ce2984c9062534c75a0c36e56b821
Jason Sherman <jsn.sherman@gmail.com>
2019-06-10 16:31:13 -0500

    more --cache-from in travis.

--- .travis/docker_compose.sh
+++ .travis/docker_compose.sh
@@ -5,8 +5,8 @@ docker pull "wikipedialibrary/twlight_base:${TRAVIS_BRANCH}" || true
 docker pull "wikipedialibrary/twlight_build:${TRAVIS_BRANCH}" || true
 
 docker build --cache-from "wikipedialibrary/twlight_base:${TRAVIS_BRANCH}" --target twlight_base --tag "twlight_base" .
-docker build --cache-from "wikipedialibrary/twlight_build:${TRAVIS_BRANCH}" --target twlight_build --tag "twlight_build" .
+docker build --cache-from "wikipedialibrary/twlight_base:${TRAVIS_BRANCH}" --cache-from "wikipedialibrary/twlight_build:${TRAVIS_BRANCH}" --target twlight_build --tag "twlight_build" .
 docker build --cache-from "twlight_base" --target twlight_base --tag "wikipedialibrary/twlight_base:${TRAVIS_BRANCH}" .
-docker build --cache-from "twlight_build" --target twlight_build --tag "wikipedialibrary/twlight_build:${TRAVIS_BRANCH}" .
+docker build --cache-from "twlight_base" --cache-from "twlight_build" --target twlight_build --tag "wikipedialibrary/twlight_build:${TRAVIS_BRANCH}" .
 docker build --cache-from "twlight_base" --cache-from "twlight_build"  --tag "wikipedialibrary/twlight:${TRAVIS_BRANCH}" .
 docker-compose -f docker-compose.yml -f docker-compose.travis.yml up -d db twlight
