caa4f643bec5e49397376b7a34f4f32e38a970cd
Jason Sherman <jsn.sherman@gmail.com>
2019-06-10 16:21:49 -0500

    try caching from build stage in travis image build

--- .travis/docker_compose.sh
+++ .travis/docker_compose.sh
@@ -5,4 +5,5 @@ docker build --cache-from "wikipedialibrary/twlight_base:${TRAVIS_BRANCH}" --tar
 docker build --cache-from "wikipedialibrary/twlight_build:${TRAVIS_BRANCH}" --target twlight_build --tag "twlight_build" .
 docker build --cache-from "twlight_base" --target twlight_base --tag "wikipedialibrary/twlight_base:${TRAVIS_BRANCH}" .
 docker build --cache-from "twlight_build" --target twlight_build --tag "wikipedialibrary/twlight_build:${TRAVIS_BRANCH}" .
+docker build --cache-from "twlight_build"  --tag "wikipedialibrary/twlight:${TRAVIS_BRANCH}" .
 docker-compose -f docker-compose.yml -f docker-compose.travis.yml up -d db twlight
