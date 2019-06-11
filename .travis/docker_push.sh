298104332c3a8d38753c336d863293af23a93269
Jason Sherman <jsn.sherman@gmail.com>
2019-06-11 10:28:27 -0500

    make sure to push docker images with our fancy new tags.

--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -4,7 +4,15 @@
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ -z "${TRAVIS_TAG}" ] && [ "${TRAVIS_BRANCH}" != "master" ]
 then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+  docker push wikipedialibrary/twlight_base:commit_${TRAVIS_COMMIT}
   docker push wikipedialibrary/twlight_base:branch_${TRAVIS_BRANCH}
+  docker push wikipedialibrary/twlight_base:build_${TRAVIS_BUILD_NUMBER}
+
+  docker push wikipedialibrary/twlight_build:commit_${TRAVIS_COMMIT}
   docker push wikipedialibrary/twlight_build:branch_${TRAVIS_BRANCH}
+  docker push wikipedialibrary/twlight_build:build_${TRAVIS_BUILD_NUMBER}
+
+  docker push wikipedialibrary/twlight:commit_${TRAVIS_COMMIT}
   docker push wikipedialibrary/twlight:branch_${TRAVIS_BRANCH}
+  docker push wikipedialibrary/twlight:build_${TRAVIS_BUILD_NUMBER}
 fi
