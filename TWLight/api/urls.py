9b067c328a163aed853fd6c4de51327266d6c372
Sam Walton <samwalton9@gmail.com>
2019-06-28 16:32:47 +0100

    Start at v0

--- TWLight/api/urls.py
+++ TWLight/api/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from TWLight.users import views
 
 urlpatterns = [
-    url(r'^(?P<version>(v1))/users/authorizations/partner/(?P<pk>\d+)/$',
+    url(r'^(?P<version>(v0))/users/authorizations/partner/(?P<pk>\d+)/$',
         views.AuthorizedUsers.as_view())
     ]
