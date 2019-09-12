04bf7df8fd4904f6d081326a3dbc06c7db580a22
Jason Sherman <jsn.sherman@gmail.com>
2019-09-12 09:20:20 -0500

    paint ezproxy code black.

--- TWLight/ezproxy/urls.py
+++ TWLight/ezproxy/urls.py
@@ -4,12 +4,14 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    url(r'^u/(?P<url>http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)$',
+    url(
+        r"^u/(?P<url>http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)$",
         login_required(views.EZProxyAuth.as_view()),
-        name='ezproxy_auth_u'
-        ),
-    url(r'^r/(?P<token>ezp\.([a-zA-Z]|[0-9]|[$-_@.&+])+)$',
+        name="ezproxy_auth_u",
+    ),
+    url(
+        r"^r/(?P<token>ezp\.([a-zA-Z]|[0-9]|[$-_@.&+])+)$",
         login_required(views.EZProxyAuth.as_view()),
-        name='ezproxy_auth_r'
-        ),
+        name="ezproxy_auth_r",
+    ),
 ]
--- TWLight/ezproxy/views.py
+++ TWLight/ezproxy/views.py
@@ -6,7 +6,11 @@ import urllib
 from time import gmtime
 from calendar import timegm
 from django.conf import settings
-from django.core.exceptions import ObjectDoesNotExist, SuspiciousOperation, ValidationError
+from django.core.exceptions import (
+    ObjectDoesNotExist,
+    SuspiciousOperation,
+    ValidationError,
+)
 from django.core.validators import URLValidator
 from django.http import HttpResponseRedirect
 from django.views import View
@@ -14,64 +18,68 @@ from TWLight.users.models import Authorization
 
 
 class EZProxyAuth(View):
+    @staticmethod
+    def get(request, url=None, token=None):
+        username = request.user.editor.wp_username
+        groups = ["Default"]
 
-  @staticmethod
-  def get(request, url=None, token=None):
-      username = request.user.editor.wp_username
-      groups = ['Default']
+        if not username:
+            raise SuspiciousOperation("Missing Editor username.")
 
-      if not username:
-          raise SuspiciousOperation('Missing Editor username.')
+        try:
+            authorizations = Authorization.objects.filter(authorized_user=request.user)
+            for authorization in authorizations:
+                partner_id = authorization.partner_id
+                stream_id = authorization.stream_id
+                group = ""
+                if partner_id:
+                    group = "P" + repr(partner_id)
+                    if stream_id:
+                        group += "S" + repr(stream_id)
+                if group:
+                    groups.append(group)
+        except ObjectDoesNotExist:
+            pass
 
-      try:
-          authorizations = Authorization.objects.filter(authorized_user=request.user)
-          for authorization in authorizations:
-              partner_id = authorization.partner_id
-              stream_id = authorization.stream_id
-              group = ''
-              if partner_id:
-                  group = 'P' +  repr(partner_id)
-                  if stream_id:
-                      group += 'S' + repr(stream_id)
-              if group:
-                  groups.append(group)
-      except ObjectDoesNotExist:
-          pass
+        if url:
+            try:
+                validate = URLValidator(schemes=("http", "https"))
+                validate(url)
+            except ValidationError:
+                raise SuspiciousOperation("Invalid EZProxy target URL.")
+        elif token:
+            url = token
+        else:
+            raise SuspiciousOperation("Missing EZProxy target URL.")
 
-
-      if url:
-          try:
-              validate = URLValidator(schemes=('http', 'https'))
-              validate(url)
-          except ValidationError:
-              raise SuspiciousOperation('Invalid EZProxy target URL.')
-      elif token:
-          url = token
-      else:
-          raise SuspiciousOperation('Missing EZProxy target URL.')
-
-      return HttpResponseRedirect(EZProxyTicket(username, groups).url(url))
+        return HttpResponseRedirect(EZProxyTicket(username, groups).url(url))
 
 
 class EZProxyTicket(object):
 
-  starting_point_url = None
+    starting_point_url = None
 
-  def __init__(self, user, groups=None):
+    def __init__(self, user, groups=None):
 
-    ezproxy_url = settings.TWLIGHT_EZPROXY_URL
-    secret = settings.TWLIGHT_EZPROXY_SECRET
+        ezproxy_url = settings.TWLIGHT_EZPROXY_URL
+        secret = settings.TWLIGHT_EZPROXY_SECRET
 
-    if not secret:
-      raise SuspiciousOperation('EZProxy Configuration Error: shared secret cannot be empty.')
+        if not secret:
+            raise SuspiciousOperation(
+                "EZProxy Configuration Error: shared secret cannot be empty."
+            )
 
-    packet = '$u' + repr(timegm(gmtime()))
-    if groups:
-      packet +=  '$g' + '+'.join(groups)
+        packet = "$u" + repr(timegm(gmtime()))
+        if groups:
+            packet += "$g" + "+".join(groups)
 
-    packet += '$e'
-    ticket = urllib.quote(hashlib.sha512(secret + user + packet).hexdigest() + packet)
-    self.starting_point_url = ezproxy_url + "/login?user=" +  user + "&ticket=" + ticket
+        packet += "$e"
+        ticket = urllib.quote(
+            hashlib.sha512(secret + user + packet).hexdigest() + packet
+        )
+        self.starting_point_url = (
+            ezproxy_url + "/login?user=" + user + "&ticket=" + ticket
+        )
 
-  def url(self, url):
-    return self.starting_point_url + "&url=" + url
+    def url(self, url):
+        return self.starting_point_url + "&url=" + url
