86fa71434aa0f0e5040f49470b618b470a895d4d
Jason Sherman <jsnshrmn@users.noreply.github.com>
2019-09-24 09:36:47 -0500

    travis only runs docker compose if build exits 0

--- .travis/docker_compose.sh
+++ .travis/docker_compose.sh
@@ -11,20 +11,19 @@ docker build --cache-from "library/alpine:latest" \
              --tag "wikipedialibrary/twlight_base:${COMMIT_TAG}"  \
              --tag "wikipedialibrary/twlight_base:${BRANCH_TAG}"  \
              --tag "wikipedialibrary/twlight_base:${BUILD_TAG}" \
-             .
+             . && \
 docker build --cache-from "wikipedialibrary/twlight_base:${BRANCH_TAG}" \
              --cache-from "wikipedialibrary/twlight_build:${BRANCH_TAG}" \
              --target twlight_build \
              --tag "wikipedialibrary/twlight_build:${COMMIT_TAG}" \
              --tag "wikipedialibrary/twlight_build:${BRANCH_TAG}" \
              --tag "wikipedialibrary/twlight_build:${BUILD_TAG}" \
-             .
+             . && \
 docker build --cache-from "wikipedialibrary/twlight_base:${BRANCH_TAG}" \
              --cache-from "wikipedialibrary/twlight_build:${BRANCH_TAG}" \
              --cache-from "wikipedialibrary/twlight:${BRANCH_TAG}"  \
              --tag "wikipedialibrary/twlight:${COMMIT_TAG}" \
              --tag "wikipedialibrary/twlight:${BRANCH_TAG}" \
              --tag "wikipedialibrary/twlight:${BUILD_TAG}" \
-             .
-
+             . && \
 docker-compose -f docker-compose.yml -f docker-compose.travis.yml up -d db twlight
