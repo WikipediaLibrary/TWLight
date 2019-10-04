70d5c2c706bdc0f2ffa705f327ce68a8db90df2d
Jason Sherman <jsnshrmn@users.noreply.github.com>
2019-10-04 07:50:31 -0500

    capture initial notes on application import format

--- TWLight/applications/management/commands/application_import.py
+++ TWLight/applications/management/commands/application_import.py
@@ -15,6 +15,16 @@ from ....resources.models import Partner, Stream
 logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
+    """
+    Imports applications from CSV.
+    Fields:
+    Partner (int: partner.id1),
+    Timestamp (date: dd/mm/yyyy),
+    Username (string: wp_username),
+    Email (string: user@example.com),
+    Collection (comma separated ints: "stream.id1, stream.id2, stream.id3"),
+    Title(semicolon separated strings: "specific_title1; specific_title2; specific_title3")
+    """
     # Let's not send mails about imported stuff.
     django.conf.settings.EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
