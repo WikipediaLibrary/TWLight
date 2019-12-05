23ad4cc0b80a027c218801c585c121864bd4d2ef
uyscuti-wiki <uyscuti.wiki@gmail.com>
2019-12-05 13:28:19 +0530

    catch all clause

--- TWLight/applications/views.py
+++ TWLight/applications/views.py
@@ -936,7 +936,8 @@ class EvaluateApplicationView(NotDeleted, CoordinatorOrSelf, ToURequired, Update
                     specific_stream=app.specific_stream
                 )
             if (
-                total_accounts_available_for_distribution > 0
+                app.partner.status != Partner.WAITLIST
+                and total_accounts_available_for_distribution > 0
                 and total_accounts_available_for_distribution -
                 total_pending_apps.count() < 0
             ):
