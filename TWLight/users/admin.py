67585daacfdeb3fba9e6a081e626b948296a96b0
Sam Walton <samwalton9@gmail.com>
2020-01-17 14:01:25 +0000

    Unused import

--- TWLight/users/admin.py
+++ TWLight/users/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from django.contrib import admin, messages
+from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
