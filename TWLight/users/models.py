31ac1fcf1c46dfbcf603d51e0d2a7e1fb765c936
Jason Sherman <jsn.sherman@gmail.com>
2020-01-24 09:21:56 -0600

    Remove leftover null ops from TWLight/users/models.py

--- TWLight/users/models.py
+++ TWLight/users/models.py
@@ -135,7 +135,6 @@ def editor_reg_date(identity, global_userinfo):
             ).date()
         except (TypeError, ValueError):
             reg_date = None
-            pass
     return reg_date
 
 
@@ -473,12 +472,10 @@ class Editor(models.Model):
             except AssertionError:
                 logger.exception("Could not fetch global_userinfo for User.")
                 return None
-                pass
 
         except:
             logger.exception("Could not fetch global_userinfo for User.")
             return None
-            pass
 
     def update_from_wikipedia(self, identity, lang):
         """
@@ -542,7 +539,6 @@ class Editor(models.Model):
                 # Email isn't guaranteed to be present in identity - don't do
                 # anything if we can't find it.
                 logger.exception("Unable to get Editor email address from Wikipedia.")
-                pass
 
         self.wp_registered = editor_reg_date(identity, global_userinfo)
         self.wp_account_old_enough = editor_account_old_enough(self.wp_registered)
