67a33e93c1a3a05cbd7d17b32c1bfac5a6d3d3b0
Sam Walton <samwalton9@gmail.com>
2020-02-28 16:57:03 +0000

    BUNDLE proxy URLs for Partners as well as Streams

--- TWLight/resources/models.py
+++ TWLight/resources/models.py
@@ -535,7 +535,11 @@ class Partner(models.Model):
     def get_access_url(self):
         ezproxy_url = settings.TWLIGHT_EZPROXY_URL
         access_url = None
-        if self.authorization_method == self.PROXY and ezproxy_url and self.target_url:
+        if(
+            self.authorization_method in [self.PROXY, self.BUNDLE]
+            and ezproxy_url
+            and self.target_url
+        ):
             access_url = ezproxy_url + "/login?url=" + self.target_url
         elif self.target_url:
             access_url = self.target_url
