7190195ae29df86eb173c9df20fca2536400ae78
Jason Sherman <jsnshrmn@users.noreply.github.com>
2020-03-17 13:06:13 -0500

    capture work on migrating authorization partner field from foreignkey to manytomany in ezproxy app.

--- TWLight/ezproxy/views.py
+++ TWLight/ezproxy/views.py
@@ -50,7 +50,7 @@ class EZProxyAuth(ToURequired, View):
                 authorization.is_valid
                 and authorization.get_authorization_method() == Partner.PROXY
             ):
-                group = "P" + repr(authorization.partner_id)
+                group = "P" + repr(authorization.partners.first().pk)
                 if authorization.stream:
                     group += "S" + repr(authorization.stream_id)
                 groups.append(group)
