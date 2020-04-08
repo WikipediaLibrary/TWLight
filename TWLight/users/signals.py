903e97d7ed15bfaf094e8dbe0890bc136b0ea3ae
Jason Sherman <jsnshrmn@users.noreply.github.com>
2020-04-08 09:22:07 -0500

    cleanup field excludsion logic in validate_authorization.

--- TWLight/users/signals.py
+++ TWLight/users/signals.py
@@ -26,16 +26,18 @@ class ProxyBundleLaunch(object):
 @receiver(pre_save, sender=Authorization)
 def validate_authorization(sender, instance, **kwargs):
     """Auths may only relate to partners with the same auth method. Only one non-bundle partner allowed."""
+    exclude = []
     # ManyToMany relationships can only exist if the instance is in the db. Those will have a pk.
-    if not instance.pk or (
-        # TODO: Investigate why we get an authorizer does not exist validation error, though we can fetch the user object.
-        # django.core.exceptions.ValidationError: {'authorizer': ['user instance with id 1 does not exist.']}
+    if not instance.pk:
+        exclude.append("partners")
+    # TODO: Investigate why we get an authorizer does not exist validation error, though we can fetch the user object.
+    # django.core.exceptions.ValidationError: {'authorizer': ['user instance with id 1 does not exist.']}
+    if (
         instance.authorizer.username == "TWL Team"
         and User.objects.filter(pk=instance.authorizer.pk).exists()
     ):
-        instance.full_clean(exclude=["authorizer"])
-    else:
-        instance.full_clean()
+        exclude.append("authorizer")
+    instance.full_clean(exclude=exclude)
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
