0cd6ddc6c59633a2c3e598312e508c904ca1061c
Sam Walton <samwalton9@gmail.com>
2020-05-06 13:16:06 +0100

    Create/expire bundle auths when appropriate

--- TWLight/users/management/commands/user_update_eligibility.py
+++ TWLight/users/management/commands/user_update_eligibility.py
@@ -73,3 +73,5 @@ class Command(BaseCommand):
                     editor.wp_valid, editor.wp_enough_recent_edits
                 )
                 editor.save()
+
+                editor.update_bundle_authorization()
