4978d6b13a057d34d66a571067fb1b9607fe2432
Jason Sherman <jsnshrmn@users.noreply.github.com>
2020-05-08 10:26:19 -0500

    streamline get_all_bundle_authorizations

--- TWLight/users/helpers/bundle_authorizations.py
+++ TWLight/users/helpers/bundle_authorizations.py
@@ -8,14 +8,8 @@ def get_all_bundle_authorizations():
     """
     Returns all Bundle authorizations, both active
     and not.
-    If no authorizations are found, returns None.
     """
 
-    auths = Authorization.objects.filter(
+    return Authorization.objects.filter(
         partners__authorization_method=Partner.BUNDLE
     ).distinct()
-
-    if auths.exists():
-        return auths
-    else:
-        return None
