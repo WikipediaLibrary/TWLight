86196674b58bf5709bb11a801679b1409efd3f80
Jason Sherman <jsnshrmn@users.noreply.github.com>
2020-05-10 13:59:20 -0500

    HOTFIX: docker-compose v1.25.5

--- bin/debian_swarm_deploy.sh
+++ bin/debian_swarm_deploy.sh
@@ -12,7 +12,7 @@ apt update && apt upgrade -y
 
 # Install docker
 apt install -y docker-ce docker-ce-cli containerd.io
-curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # Add twlight user
