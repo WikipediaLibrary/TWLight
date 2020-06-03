8ac8e0bf536ce96f5d0f9ae2dc08b21a5f5be6ed
Sam Walton <samwalton9@gmail.com>
2020-06-03 13:07:38 +0100

    Update example resources with link, proxy, and bundle auth methods

--- TWLight/resources/management/commands/resources_example_data.py
+++ TWLight/resources/management/commands/resources_example_data.py
@@ -36,27 +36,54 @@ class Command(BaseCommand):
         ]
 
         coordinators = User.objects.filter(groups__name="coordinators")
+        non_proxy_auth_methods = [Partner.EMAIL, Partner.CODES, Partner.LINK]
+        non_bundle_auth_methods = non_proxy_auth_methods + [Partner.PROXY]
+        all_auth_methods = non_bundle_auth_methods + [Partner.BUNDLE]
 
         for _ in range(num_partners):
+            # Choose a random authorization method for this partner
+            auth_method = random.choice(all_auth_methods)
+
+            # Basic information that all partners will have
             partner = PartnerFactory(
                 company_location=random.choice(list(countries)),
-                renewals_available=random.choice([True, False]),
                 short_description=Faker(
                     random.choice(settings.FAKER_LOCALES)
                 ).paragraph(nb_sentences=4),
-                send_instructions=Faker(
-                    random.choice(settings.FAKER_LOCALES)
-                ).paragraph(nb_sentences=2),
-                coordinator=random.choice(coordinators),
-                real_name=self.chance(True, False, 40),
-                country_of_residence=self.chance(True, False, 20),
-                specific_title=self.chance(True, False, 10),
-                specific_stream=self.chance(True, False, 10),
-                occupation=self.chance(True, False, 10),
-                affiliation=self.chance(True, False, 10),
-                agreement_with_terms_of_use=self.chance(True, False, 10),
                 mutually_exclusive=False,
+                authorization_method=auth_method
             )
+
+            # We only have send instructions if this isn't a proxy partner
+            if auth_method in non_proxy_auth_methods:
+                partner.send_instructions = Faker(
+                    random.choice(settings.FAKER_LOCALES)
+                ).paragraph(nb_sentences=2)
+
+            # Bundle partners don't require extra information, but others might.
+            # They also have coordinators, can have streams, and have renewals
+            if auth_method in non_bundle_auth_methods:
+                partner.real_name = self.chance(True, False, 40)
+                partner.country_of_residence = self.chance(True, False, 20)
+                partner.specific_title = self.chance(True, False, 10)
+                partner.occupation = self.chance(True, False, 10)
+                partner.affiliation = self.chance(True, False, 10)
+                partner.agreement_with_terms_of_use = self.chance(True, False, 10)
+                partner.coordinator = random.choice(coordinators)
+                partner.specific_stream = self.chance(True, False, 10)
+                partner.renewals_available = random.choice([True, False])
+
+            # Partners accessed via proxy need a target URL and requested_access_duration=True
+            if auth_method not in non_proxy_auth_methods:
+                partner.target_url = "https://example.com"
+            if auth_method == Partner.PROXY:
+                partner.requested_access_duration = True
+
+            # LINK and CODES partners need email instructions
+            if auth_method in [Partner.CODES, Partner.LINK]:
+                partner.user_instructions = "User instructions email text."
+
+            partner.save()
 
             # ManyToMany relationships can't be set until the partner object has
             # been created.
@@ -74,10 +101,17 @@ class Command(BaseCommand):
             for tag in random.sample(tag_list, random.randint(1, 4)):
                 partner.tags.add(tag)
 
+        all_non_proxy_partners = Partner.objects.filter(
+            authorization_method__in=non_proxy_auth_methods
+        )
+        all_non_bundle_partners = Partner.objects.filter(
+            authorization_method__in=non_bundle_auth_methods
+        )
+
         # Set 5 partners to need a registration URL. We do this separately
         # because it requires both the account_email and registration_url
         # fields to be set concurrently.
-        for registration_partner in random.sample(list(all_partners), 5):
+        for registration_partner in random.sample(list(all_non_proxy_partners), 5):
             registration_partner.account_email = True
             registration_partner.registration_url = Faker(
                 random.choice(settings.FAKER_LOCALES)
@@ -97,7 +131,7 @@ class Command(BaseCommand):
             words.excerpt_limit = random.randint(100, 250)
             words.save()
 
-        # Set 5% random partners to have excerpt limit in words
+        # Set 5% random partners to have excerpt limit in percentage
         for percentage in random.sample(list(all_partners), 10):
             percentage.excerpt_limit_percentage = random.randint(5, 50)
             percentage.save()
@@ -111,7 +145,7 @@ class Command(BaseCommand):
         available_partners = all_partners.exclude(status=Partner.NOT_AVAILABLE)
 
         # Set 10 random available partners to be waitlisted
-        for waitlisted_partner in random.sample(list(available_partners), 10):
+        for waitlisted_partner in random.sample(list(all_non_proxy_partners), 10):
             waitlisted_partner.status = Partner.WAITLIST
             waitlisted_partner.save()
 
@@ -123,18 +157,18 @@ class Command(BaseCommand):
             long_description.save()
 
         # Set 10 random available partners to be featured
-        for featured_partner in random.sample(list(available_partners), 10):
+        for featured_partner in random.sample(list(all_non_bundle_partners), 10):
             featured_partner.featured = True
             featured_partner.save()
 
-        # Give any specific_stream flagged partners streams.
-        stream_partners = all_partners.filter(specific_stream=True)
-
         # Random number of accounts available for all partners without streams
-        for accounts in all_partners:
+        for accounts in all_non_bundle_partners:
             if not accounts.specific_stream:
                 accounts.accounts_available = random.randint(10, 550)
                 accounts.save()
+
+        # Give any specific_stream flagged partners streams.
+        stream_partners = all_partners.filter(specific_stream=True)
 
         # If we happened to not create any partners with streams,
         # create one deliberately.
@@ -144,6 +178,7 @@ class Command(BaseCommand):
             stream_partners[0].save()
 
         for partner in stream_partners:
+            # Add 3 streams for each partner with streams
             for _ in range(3):
                 stream = StreamFactory(
                     partner=partner,
@@ -155,6 +190,8 @@ class Command(BaseCommand):
                     description=Faker(random.choice(settings.FAKER_LOCALES)).paragraph(
                         nb_sentences=2
                     ),
+                    # Authorization method needs to be the same as partner
+                    authorization_method=partner.authorization_method
                 )
 
         # Set 15 partners to have somewhere between 1 and 5 video tutorial URLs
@@ -173,7 +210,7 @@ class Command(BaseCommand):
             each_stream.accounts_available = random.randint(10, 100)
             each_stream.save()
 
-        # Generate a few number of suggestions with upvotes
+        # Generate a few partner suggestions with upvotes
         all_users = User.objects.exclude(is_superuser=True)
         author_user = random.choice(all_users)
         for _ in range(random.randint(3, 10)):
@@ -188,11 +225,8 @@ class Command(BaseCommand):
             random_users = random.sample(list(all_users), random.randint(1, 10))
             suggestion.upvoted_users.add(*random_users)
 
-        # Set 5 partners use the access code authorization method,
-        # and generate a bunch of codes for each.
-        for partner in random.sample(list(available_partners), 5):
-            partner.authorization_method = Partner.CODES
-            partner.save()
+        # Generate access codes for CODES partners
+        for partner in Partner.objects.filter(authorization_method=Partner.CODES):
 
             for i in range(25):
                 new_access_code = AccessCode()
