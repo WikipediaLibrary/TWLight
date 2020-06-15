548c045dce250fd06eb69d13d9d48a7a1b81f12e
Jason Sherman <jsn.sherman@gmail.com>
2020-06-15 11:41:50 -0500

    add smarter deploy script to be called from crontab.

---
+++ bin/twlight_docker_deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Move into the repository and determine the git branch.
+cd /srv/TWLight && branch=$(git rev-parse --abbrev-ref HEAD)
+# Check for newer image
+if [ -n "${branch+isset}" ] && docker pull wikipedialibrary/twlight:branch_${branch} | grep "Status: Downloaded newer image for wikipedialibrary/twlight    :branch_${branch}" >/dev/null
+then
+    # Get any new docker-compose or script updates.
+    git pull
+    # Deploy the stack
+    docker stack deploy -c docker-compose.yml -c docker-compose.${branch}.yml ${branch}
+# Report if the local image is already up to date.
+elif [ -n "${branch+isset}" ] && docker pull wikipedialibrary/twlight:branch_${branch} | grep "Status: Image is up to date for wikipedialibrary/twlight:branch_${branch}" >/dev/null
+then
+   echo "Up to date"
+# Fail in any other circumstance.
+else
+   echo "Error"
+   exit 1
+fi
