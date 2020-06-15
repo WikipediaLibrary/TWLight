a90f6eb1247f18f2b4a374152a103fc3405f84b9
Jason Sherman <jsn.sherman@gmail.com>
2020-06-15 13:44:01 -0500

    require args for deploy script to be called from crontab

--- bin/twlight_docker_deploy.sh
+++ bin/twlight_docker_deploy.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
-# Move into the repository and determine the git branch.
-cd /srv/TWLight && branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [ -z "$1" ] || [ -z "$2" ]
+then
+    echo "Usage: twlight_docker_deploy.sh \$env \$tag
+    \$env    docker swarm environment (eg. staging \| production).
+    \$tag    docker hub image tag (eg. branch_staging \| branch_production \| latest)"
+    exit 1;
+fi
+
+env=${1}
+tag=${2}
+
+# Move into the repository.
+cd /srv/TWLight
 # Check for newer image
-if [ -n "${branch+isset}" ] && docker pull wikipedialibrary/twlight:branch_${branch} | grep "Status: Downloaded newer image for wikipedialibrary/twlight:branch_${branch}" >/dev/null
+if docker pull wikipedialibrary/twlight:${tag} | grep "Status: Downloaded newer image for wikipedialibrary/twlight:${tag}" >/dev/null
 then
     # Get any new docker-compose or script updates.
     git pull
     # Deploy the stack
-    docker stack deploy -c docker-compose.yml -c docker-compose.${branch}.yml ${branch}
+    docker stack deploy -c docker-compose.yml -c docker-compose.${env}.yml ${env}
 # Report if the local image is already up to date.
-elif [ -n "${branch+isset}" ] && docker pull wikipedialibrary/twlight:branch_${branch} | grep "Status: Image is up to date for wikipedialibrary/twlight:branch_${branch}" >/dev/null
+elif docker pull wikipedialibrary/twlight:${tag} | grep "Status: Image is up to date for wikipedialibrary/twlight:${tag}" >/dev/null
 then
    echo "Up to date"
 # Fail in any other circumstance.
