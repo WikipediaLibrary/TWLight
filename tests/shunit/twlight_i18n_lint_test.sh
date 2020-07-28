be9d77a5b84b0edb2a7d022eb0318979cca65f12
Jason Sherman <jsn.sherman@gmail.com>
2020-07-28 08:25:49 -0500

    Name 'good' i18n test data files more logically. Both the newlines and the comments are correct in them.

--- tests/shunit/twlight_i18n_lint_test.sh
+++ tests/shunit/twlight_i18n_lint_test.sh
@@ -12,12 +12,12 @@ testBadPyNewlines() {
     done
 }
 
-testGoodPyComments() {
+testGoodPy() {
     start=1
     end=2
     for ((i=$start; i<=$end; i++))
     do
-        file=${TWLIGHT_HOME}/tests/shunit/data/good_i18n_comment_$i.py
+        file=${TWLIGHT_HOME}/tests/shunit/data/good_i18n_$i.py
         assertTrue "${file} should not cause an error." "perl ${TWLIGHT_HOME}/bin/twlight_i18n_lint.pl ${file}" ||:
     done
 }
