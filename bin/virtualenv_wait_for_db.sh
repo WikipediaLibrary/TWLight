1a6eb7a4e1545f56d2e84f44da9ce62446605935
suecarmol <scardenasmolinar@wikimedia.org>
2020-09-23 18:34:22 -0500

    Add more verbose error message when DB times out

--- bin/virtualenv_wait_for_db.sh
+++ bin/virtualenv_wait_for_db.sh
@@ -8,18 +8,32 @@ then
     # Try to get a db shell
     db_init_wait=0
     db_init_timeout=60
-    until echo 'exit' | python3 ${TWLIGHT_HOME}/manage.py dbshell 2>/dev/null || [ $db_init_wait -eq $db_init_timeout ]
+    function connect() {
+        connect=$(echo 'exit' | python3 ${TWLIGHT_HOME}/manage.py dbshell 2>/dev/null)
+        if ${connect} 2>/dev/null
+        then
+            true
+        else
+            echo ${connect} | sed -e "s/'--\(user\|password\)=[^']*'/'--\1=******'/g" >/tmp/twlight_db_connect
+            false
+        fi
+    }
+
+    until connect || [ $db_init_wait -eq $db_init_timeout ]
     do
         >&2 echo "Waiting for DB."
         sleep 1
-        db_init_wait=$(( ++db_init_wait ))
+        db_init_wait=$(( $db_init_wait + 1 ))
     done
 
     if [ $db_init_wait -lt $db_init_timeout ]
     then
         >&2 echo "DB up."
+        rm /tmp/twlight_db_connect 2>/dev/null || :
+    	exec "$@"
     else
-        echo "DB timeout."
+        cat /tmp/twlight_db_connect
+        rm /tmp/twlight_db_connect 2>/dev/null || :
         exit 1
     fi
 else
