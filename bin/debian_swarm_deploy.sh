008bfe9d05f529eab276515540d4fa0080e0a29a
Jason Sherman <jsn.sherman@gmail.com>
2020-11-09 07:53:56 -0600

    install latest docker compose in debian_swarm_deploy.sh.

--- bin/debian_swarm_deploy.sh
+++ bin/debian_swarm_deploy.sh
@@ -12,7 +12,10 @@ apt update && apt upgrade -y
 
 # Install docker
 apt install -y docker-ce docker-ce-cli containerd.io
-curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Install docker compose
+latest=$(curl https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+curl -L "https://github.com/docker/compose/releases/download/${latest}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # Add twlight user
