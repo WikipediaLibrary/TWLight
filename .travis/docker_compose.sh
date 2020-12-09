1f326eafe8ed5845a2ed3c998fe7392cfa27e2e8
Jason Sherman <jsn.sherman@gmail.com>
2020-12-09 12:41:55 -0600

    Replace docker.io with quay.io for alpine base image.

--- .travis/docker_compose.sh
+++ .travis/docker_compose.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+# Pull alpine base image from docker hub, but then retag it for $cr_server for reuse.
 docker pull "library/alpine:3.11" || true
+docker tag "library/alpine:3.11" "${cr_server}/wikipedialibrary/alpine:3.11"
 docker pull "${cr_server}/wikipedialibrary/twlight_base:${BRANCH_TAG}" || true
 docker pull "${cr_server}/wikipedialibrary/twlight_build:${BRANCH_TAG}" || true
 docker pull "${cr_server}/wikipedialibrary/twlight:${BRANCH_TAG}" || true
 
-docker build --cache-from "library/alpine:3.11" \
+docker build --cache-from "${cr_server}/wikipedialibrary/alpine:3.11" \
              --cache-from "${cr_server}/wikipedialibrary/twlight_base:${BRANCH_TAG}" \
              --target twlight_base \
              --tag "${cr_server}/wikipedialibrary/twlight_base:${COMMIT_TAG}"  \
--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -13,6 +13,9 @@ echo "TWLIGHT_TRANSLATION_FILES_CHANGED: ${TWLIGHT_TRANSLATION_FILES_CHANGED}"
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ -z "${TRAVIS_TAG}" ] && [ -n "${cr_server+isset}" && [ -n "${cr_username+isset}" ] && [ -n "${cr_password+isset}" ] && [ -n "${TWLIGHT_MISSING_MIGRATIONS+isset}" ] && [ "${TWLIGHT_MISSING_MIGRATIONS}" -eq 0 ] && [ "${TWLIGHT_TRANSLATION_FILES_CHANGED}" -eq 0 ]
 then
   echo "$cr_password" | docker login $cr_server -u "$cr_username" --password-stdin
+
+  docker push ${cr_server}/wikipedialibrary/alpine:3.11
+
   docker push ${cr_server}/wikipedialibrary/twlight_base:${COMMIT_TAG}
   docker push ${cr_server}/wikipedialibrary/twlight_base:${BRANCH_TAG}
   docker push ${cr_server}/wikipedialibrary/twlight_base:${BUILD_TAG}
