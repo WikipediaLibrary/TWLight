f00192ed55e9182c942d59c81201f466e5a9ef80
jsn <jsherman@wikimedia.org>
2021-05-06 13:27:06 -0500

    handle image caching directly in docker-compose

--- .travis/docker_compose.sh
+++ .travis/docker_compose.sh
@@ -1,43 +1,36 @@
 #!/usr/bin/env bash
+export DOCKER_BUILDKIT=1 # enable buildkit for parallel builds
+export COMPOSE_DOCKER_CLI_BUILD=1 # use docker cli for building
 
 # Pull images from docker hub, but then retag for $cr_server for reuse and mirroring.
 docker pull "docker.io/library/alpine:3.11" || true
-docker tag "docker.io/library/alpine:3.11" "${cr_server}/wikipedialibrary/alpine:3.11"
+docker tag "docker.io/library/alpine:3.11" "quay.io/wikipedialibrary/alpine:3.11"
 docker pull "docker.io/library/mariadb:10" || true
-docker tag "docker.io/library/mariadb:10" "${cr_server}/wikipedialibrary/mariadb:10"
+docker tag "docker.io/library/mariadb:10" "quay.io/wikipedialibrary/mariadb:10"
 docker pull "docker.io/library/nginx:latest" || true
-docker tag "docker.io/library/nginx:latest" "${cr_server}/wikipedialibrary/nginx:latest"
+docker tag "docker.io/library/nginx:latest" "quay.io/wikipedialibrary/nginx:latest"
 
 # Build images with caching and then run the stack in docker-compose.
-docker pull "${cr_server}/wikipedialibrary/twlight_base:${BRANCH_TAG}" || true
-docker pull "${cr_server}/wikipedialibrary/twlight_build:${BRANCH_TAG}" || true
-docker pull "${cr_server}/wikipedialibrary/twlight:${BRANCH_TAG}" || true
-docker pull "${cr_server}/wikipedialibrary/twlight_syslog:${BRANCH_TAG}" || true
+docker pull "quay.io/wikipedialibrary/twlight_base:branch_production" || true
+docker pull "quay.io/wikipedialibrary/twlight_build:branch_production" || true
+docker pull "quay.io/wikipedialibrary/twlight:branch_production" || true
+docker pull "quay.io/wikipedialibrary/twlight_syslog:branch_production" || true
 
-docker build --cache-from "${cr_server}/wikipedialibrary/alpine:3.11" \
-             --cache-from "${cr_server}/wikipedialibrary/twlight_base:${BRANCH_TAG}" \
-             --target twlight_base \
-             --tag "${cr_server}/wikipedialibrary/twlight_base:${COMMIT_TAG}"  \
-             --tag "${cr_server}/wikipedialibrary/twlight_base:${BRANCH_TAG}"  \
-             --tag "${cr_server}/wikipedialibrary/twlight_base:${BUILD_TAG}" \
-             . && \
-docker build --cache-from "${cr_server}/wikipedialibrary/twlight_base:${BRANCH_TAG}" \
-             --cache-from "${cr_server}/wikipedialibrary/twlight_build:${BRANCH_TAG}" \
-             --target twlight_build \
-             --tag "${cr_server}/wikipedialibrary/twlight_build:${COMMIT_TAG}" \
-             --tag "${cr_server}/wikipedialibrary/twlight_build:${BRANCH_TAG}" \
-             --tag "${cr_server}/wikipedialibrary/twlight_build:${BUILD_TAG}" \
-             . && \
-docker build --cache-from "${cr_server}/wikipedialibrary/twlight_base:${BRANCH_TAG}" \
-             --cache-from "${cr_server}/wikipedialibrary/twlight_build:${BRANCH_TAG}" \
-             --cache-from "${cr_server}/wikipedialibrary/twlight:${BRANCH_TAG}"  \
-             --tag "${cr_server}/wikipedialibrary/twlight:${COMMIT_TAG}" \
-             --tag "${cr_server}/wikipedialibrary/twlight:${BRANCH_TAG}" \
-             --tag "${cr_server}/wikipedialibrary/twlight:${BUILD_TAG}" \
-             . && \
-docker build --cache-from "${cr_server}/wikipedialibrary/twlight_syslog:${BRANCH_TAG}" \
-             --tag "${cr_server}/wikipedialibrary/twlight_syslog:${COMMIT_TAG}" \
-             --tag "${cr_server}/wikipedialibrary/twlight_syslog:${BRANCH_TAG}" \
-             --tag "${cr_server}/wikipedialibrary/twlight_syslog:${BUILD_TAG}" \
-             syslog && \
-docker-compose -f docker-compose.yml -f docker-compose.travis.yml up -d db twlight
+docker-compose build twlight_base
+docker tag "quay.io/wikipedialibrary/twlight_base:local" "quay.io/wikipedialibrary/twlight_base:${BRANCH_TAG}"
+docker tag "quay.io/wikipedialibrary/twlight_base:local" "quay.io/wikipedialibrary/twlight_base:${BUILD_TAG}"
+docker tag "quay.io/wikipedialibrary/twlight_base:local" "quay.io/wikipedialibrary/twlight_base:${COMMIT_TAG}"
+docker-compose build twlight_build
+docker tag "quay.io/wikipedialibrary/twlight_build:local" "quay.io/wikipedialibrary/twlight_build:${BRANCH_TAG}"
+docker tag "quay.io/wikipedialibrary/twlight_build:local" "quay.io/wikipedialibrary/twlight_build:${BUILD_TAG}"
+docker tag "quay.io/wikipedialibrary/twlight_build:local" "quay.io/wikipedialibrary/twlight_build:${COMMIT_TAG}"
+docker-compose build twlight
+docker tag "quay.io/wikipedialibrary/twlight:local" "quay.io/wikipedialibrary/twlight:${BRANCH_TAG}"
+docker tag "quay.io/wikipedialibrary/twlight:local" "quay.io/wikipedialibrary/twlight:${BUILD_TAG}"
+docker tag "quay.io/wikipedialibrary/twlight:local" "quay.io/wikipedialibrary/twlight:${COMMIT_TAG}"
+docker-compose build syslog
+docker tag "quay.io/wikipedialibrary/twlight_syslog:local" "quay.io/wikipedialibrary/twlight_syslog:${BRANCH_TAG}"
+docker tag "quay.io/wikipedialibrary/twlight_syslog:local" "quay.io/wikipedialibrary/twlight_syslog:${BUILD_TAG}"
+docker tag "quay.io/wikipedialibrary/twlight_syslog:local" "quay.io/wikipedialibrary/twlight_syslog:${COMMIT_TAG}"
+
+docker-compose up -d db twlight
