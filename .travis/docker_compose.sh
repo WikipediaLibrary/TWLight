87f9de05c6d4ab4c3205f76fdae799b2ed9fe342
jsn <jsherman@wikimedia.org>
2021-05-11 14:10:35 -0500

    add travis-specific compose file back in

--- .travis/docker_compose.sh
+++ .travis/docker_compose.sh
@@ -2,15 +2,15 @@
 export DOCKER_BUILDKIT=1 # enable buildkit for parallel builds
 export COMPOSE_DOCKER_CLI_BUILD=1 # use docker cli for building
 
-docker-compose build twlight_base
+docker-compose -f docker-compose.travis.yml build twlight_base
 docker tag "quay.io/wikipedialibrary/twlight_base:local" "quay.io/wikipedialibrary/twlight_base:${BRANCH_TAG}"
 docker tag "quay.io/wikipedialibrary/twlight_base:local" "quay.io/wikipedialibrary/twlight_base:${BUILD_TAG}"
 docker tag "quay.io/wikipedialibrary/twlight_base:local" "quay.io/wikipedialibrary/twlight_base:${COMMIT_TAG}"
-docker-compose build twlight_build
+docker-compose -f docker-compose.travis.yml build twlight_build
 docker tag "quay.io/wikipedialibrary/twlight_build:local" "quay.io/wikipedialibrary/twlight_build:${BRANCH_TAG}"
 docker tag "quay.io/wikipedialibrary/twlight_build:local" "quay.io/wikipedialibrary/twlight_build:${BUILD_TAG}"
 docker tag "quay.io/wikipedialibrary/twlight_build:local" "quay.io/wikipedialibrary/twlight_build:${COMMIT_TAG}"
-docker-compose build twlight
+docker-compose -f docker-compose.travis.yml build twlight
 docker tag "quay.io/wikipedialibrary/twlight:local" "quay.io/wikipedialibrary/twlight:${BRANCH_TAG}"
 docker tag "quay.io/wikipedialibrary/twlight:local" "quay.io/wikipedialibrary/twlight:${BUILD_TAG}"
 docker tag "quay.io/wikipedialibrary/twlight:local" "quay.io/wikipedialibrary/twlight:${COMMIT_TAG}"
