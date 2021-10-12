342ce676747ac984615f1cbc70d5ce36acc62d92
suecarmol <scardenasmolinar@wikimedia.org>
2021-10-12 18:03:08 -0500

    Refactor PartnerFilter; create a new main class

--- TWLight/resources/filters.py
+++ TWLight/resources/filters.py
@@ -8,7 +8,10 @@ from .helpers import get_tag_choices
 import django_filters
 
 
-class PartnerFilter(django_filters.FilterSet):
+class MainPartnerFilter(django_filters.FilterSet):
+    """
+    This filter is for the /partners page
+    """
 
     tags = django_filters.ChoiceFilter(
         # Translators: On the MyLibrary page (https://wikipedialibrary.wmflabs.org/users/my_library), this text is shown to indicate how many subject areas a collection covers.
@@ -23,13 +26,6 @@ class PartnerFilter(django_filters.FilterSet):
         queryset=Language.objects.all(),
     )
 
-    searchable = django_filters.MultipleChoiceFilter(
-        # Translators: On the MyLibrary page (https://wikipedialibrary.wmflabs.org/users/my_library), this text is shown to indicate if a collection is searchable.
-        label=_("Searchable"),
-        choices=Partner.SEARCHABLE_CHOICES,
-        widget=forms.CheckboxSelectMultiple,
-    )
-
     def __init__(self, data=None, *args, **kwargs):
         # grab "language_code" from kwargs and then remove it so we can call super()
         language_code = None
@@ -37,17 +33,7 @@ class PartnerFilter(django_filters.FilterSet):
             language_code = kwargs.get("language_code")
             kwargs.pop("language_code")
 
-        # Set searchable to all selected by default
-        # if filterset is bound, use initial values as defaults
-        if data is not None:
-            # get a mutable copy of the QueryDict
-            data = data.copy()
-            for name, f in self.base_filters.items():
-                # filter param is either missing or empty, set all searchable values in MultiValueDict
-                if name == "searchable" and not data.get(name):
-                    data.setlist("searchable", ["0", "1", "2"])
-
-        super(PartnerFilter, self).__init__(data, *args, **kwargs)
+        super().__init__(data, *args, **kwargs)
         self.filters["tags"].extra.update({"choices": get_tag_choices(language_code)})
         # Add CSS classes to style widgets
         self.filters["tags"].field.widget.attrs.update(
@@ -55,9 +41,6 @@ class PartnerFilter(django_filters.FilterSet):
         )
         self.filters["languages"].field.widget.attrs.update(
             {"class": "form-control form-control-sm"}
-        )
-        self.filters["searchable"].field.widget.attrs.update(
-            {"class": "searchable-form"}
         )
 
     class Meta:
@@ -70,3 +53,32 @@ class PartnerFilter(django_filters.FilterSet):
             | Q(new_tags__tags__contains="multidisciplinary_tag")
         )
         return tag_filter
+
+
+class PartnerFilter(MainPartnerFilter):
+    """
+    This filter is used in the MyLibrary page
+    """
+
+    searchable = django_filters.MultipleChoiceFilter(
+        # Translators: On the MyLibrary page (https://wikipedialibrary.wmflabs.org/users/my_library), this text is shown to indicate if a collection is searchable.
+        label=_("Searchable"),
+        choices=Partner.SEARCHABLE_CHOICES,
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    def __init__(self, data=None, *args, **kwargs):
+        # Set searchable to all selected by default
+        # if filterset is bound, use initial values as defaults
+        if data is not None:
+            # get a mutable copy of the QueryDict
+            data = data.copy()
+            for name, f in self.base_filters.items():
+                # filter param is either missing or empty, set all searchable values in MultiValueDict
+                if name == "searchable" and not data.get(name):
+                    data.setlist("searchable", ["0", "1", "2"])
+
+        super().__init__(data, *args, **kwargs)
+        self.filters["searchable"].field.widget.attrs.update(
+            {"class": "searchable-form"}
+        )
