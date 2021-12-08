5e60ceb5278e6b8314a1b83ee3aa10d770c0b8ce
suecarmol <scardenasmolinar@wikimedia.org>
2021-12-07 19:11:21 -0600

    Migrate 404 error page to Bootstrap 4

--- TWLight/urls.py
+++ TWLight/urls.py
@@ -110,5 +110,5 @@ if (
         url(r"^400/$", TemplateView.as_view(template_name="400.html")),
         url(r"^403/$", TemplateView.as_view(template_name="403.html")),
         url(r"^404/$", TemplateView.as_view(template_name="404.html")),
-        url(r"^500/$", TemplateView.as_view(template_name="500.html")),
+        url(r"^500/$", TemplateView.as_view(template_name="500/500.html")),
     ]
