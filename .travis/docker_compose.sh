367bf5bf88f08f2b29ec7c04d5d99e0116dd283c
Jason Sherman <jsnshrmn@users.noreply.github.com>
2022-04-04 10:00:30 -0500

    HOTFIX: update travis docker build

--- .travis/docker_compose.sh
+++ .travis/docker_compose.sh
@@ -2,7 +2,7 @@
 export DOCKER_BUILDKIT=1 # enable buildkit for parallel builds
 export COMPOSE_DOCKER_CLI_BUILD=1 # use docker cli for building
 
-docker-compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.travis.yml build --progress=plain syslog
+docker-compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.travis.yml build twlight syslog 2>/dev/null
 docker tag "quay.io/wikipedialibrary/twlight:local" "quay.io/wikipedialibrary/twlight:${BRANCH_TAG}"
 docker tag "quay.io/wikipedialibrary/twlight:local" "quay.io/wikipedialibrary/twlight:${BUILD_TAG}"
 docker tag "quay.io/wikipedialibrary/twlight:local" "quay.io/wikipedialibrary/twlight:${COMMIT_TAG}"
