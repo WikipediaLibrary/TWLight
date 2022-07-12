26818f0472d9927ceb50e6bed7c447128fd9caeb
suecarmol <scardenasmolinar@wikimedia.org>
2022-07-12 17:46:53 -0500

    Don't serialize test database

--- TWLight/settings/base.py
+++ TWLight/settings/base.py
@@ -239,6 +239,7 @@ DATABASES = {
             "charset": "utf8mb4",
             "init_command": "SET sql_mode='STRICT_ALL_TABLES'; SET storage_engine='INNODB';",
         },
+        "TEST": {"SERIALIZE": False},
     }
 }
 
