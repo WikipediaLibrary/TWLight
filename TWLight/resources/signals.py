05823065a1c2a7474bf28e2f467cac7539188ea1
Jason Sherman <jsnshrmn@users.noreply.github.com>
2022-10-06 09:03:25 -0500

    resources: clear cache after saving PhabricatorTask

--- TWLight/resources/signals.py
+++ TWLight/resources/signals.py
@@ -1,11 +1,11 @@
 from django.core.cache import cache
 from django.dispatch import receiver, Signal
 from django.db.models.signals import pre_save, post_save, post_delete
-from TWLight.resources.models import Partner
+from TWLight.resources.models import Partner, PhabricatorTask
 
 
 @receiver(post_save, sender=Partner)
+@receiver(post_save, sender=PhabricatorTask)
 def delete_all_cache(sender, instance, **kwargs):
-    """Partner updates impact cached pages across the site, so clear all cache after saving."""
-    if sender == Partner:
-        cache.clear()
+    """Clear all cache after saving objects that impaced cached pages across the site."""
+    cache.clear()
