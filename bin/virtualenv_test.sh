acf4af9ff086aa23b8581c7ebe760ca8cfd1c59d
Jason Sherman <jsnshrmn@users.noreply.github.com>
2023-10-27 15:06:49 -0500

    cicd: black now checks for python 3.11

--- bin/virtualenv_test.sh
+++ bin/virtualenv_test.sh
@@ -12,8 +12,8 @@ set -euo pipefail
     if source ${TWLIGHT_HOME}/bin/virtualenv_activate.sh
     then
         # Run linter
-        echo "black --target-version py38 --check TWLight"
-        if black --target-version py38 --check TWLight
+        echo "black --target-version py311 --check TWLight"
+        if black --target-version py311 --check TWLight
         then
             echo "${TWLIGHT_HOME}/tests/shunit/twlight_i18n_lint_test.sh"
             ${TWLIGHT_HOME}/tests/shunit/twlight_i18n_lint_test.sh
@@ -30,9 +30,9 @@ set -euo pipefail
             coverage run --source TWLight manage.py test --keepdb --noinput --parallel --timing
         else
             # If linting fails, offer some useful feedback to the user.
-            black --target-version py38 --quiet --diff TWLight
+            black --target-version py311 --quiet --diff TWLight
             echo "You can fix these issues by running the following command on your host"
-            echo "docker exec CONTAINER $(which black) -t py38 ${TWLIGHT_HOME}/TWLight"
+            echo "docker exec CONTAINER $(which black) -t py311 ${TWLIGHT_HOME}/TWLight"
             exit 1
         fi
     else
