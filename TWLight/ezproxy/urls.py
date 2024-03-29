2fad22b698f360c2ee29674088e3d1593d531654
suecarmol <scardenasmolinar@wikimedia.org>
2024-03-28 21:33:40 -0600

    Fixed regexes in EZProxy urls.py file

--- TWLight/ezproxy/urls.py
+++ TWLight/ezproxy/urls.py
@@ -5,12 +5,12 @@ from . import views
 
 urlpatterns = [
     re_path(
-        "u/(?P<url>http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)$",
+        r"^u/(?P<url>http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)$",
         login_required(views.EZProxyAuth.as_view()),
         name="ezproxy_auth_u",
     ),
     re_path(
-        "r/(?P<token>ezp\.([a-zA-Z]|[0-9]|[$-_@.&+])+)$",
+        r"^r/(?P<token>ezp\.([a-zA-Z]|[0-9]|[$-_@.&+])+)$",
         login_required(views.EZProxyAuth.as_view()),
         name="ezproxy_auth_r",
     ),
