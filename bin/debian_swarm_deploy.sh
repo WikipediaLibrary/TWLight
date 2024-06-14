c98110ba834a7791f9349fb63a5c325271ab19eb
Jason Sherman <jsnshrmn@users.noreply.github.com>
2024-06-14 09:30:39 -0500

    debian_swarm_deploy: update for debian bookworm
    - update apt repositories
    - aim for idempodency
      - uninstall conflicting docker packages
      - delete TWLight repository if it exists
    - automate DKIM configuration
    - automate creation of some secrets
    - remove dangling Matomo lines

--- bin/debian_swarm_deploy.sh
+++ bin/debian_swarm_deploy.sh
@@ -1,90 +1,99 @@
 #!/usr/bin/env bash
 # Installs dependencies and deploys TWLight to a single Debian host.
 
-# Ensure the docker repo will be usable.
-apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-# Add the apt key
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-# Add the apt repo
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-# Update
-apt update && apt upgrade -y
+# Uninstall any conflicting packages
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc
+do
+    apt remove $pkg
+done
+
+# Add Docker's official GPG key:
+apt update
+apt install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
 
 # Install docker
-apt install -y docker-ce docker-ce-cli containerd.io
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Install other packages we require
+apt install -y cron
 
 # Cleanup unneeded packages
 apt autoremove -y
-
-# Install docker compose
-latest=$(curl https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
-curl -L "https://github.com/docker/compose/releases/download/${latest}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
 
 # Add twlight user
 adduser twlight --disabled-password --quiet --gecos "" ||:
 usermod -a -G docker twlight
 
+# Delete any existing repo
+rm -rf /srv/TWLight
 # Pull TWLight code and make twlight user the owner
-cd /srv
-git clone https://github.com/WikipediaLibrary/TWLight.git ||:
+git clone https://github.com/WikipediaLibrary/TWLight.git /srv/TWLight
 cd /srv/TWLight
 # Get on correct branch
 echo "Enter git branch:"
 read TWLIGHT_GIT_BRANCH
 git checkout "${TWLIGHT_GIT_BRANCH}" && git pull
 
-
+# Get input from human
 echo "Enter DJANGO_DB_NAME:"
 read DJANGO_DB_NAME
 echo "Enter DJANGO_DB_USER:"
 read DJANGO_DB_USER
-echo "Enter DJANGO_DB_PASSWORD:"
-read DJANGO_DB_PASSWORD
-echo "Enter DKIM_PRIVATE_KEY:"
-read DKIM_PRIVATE_KEY
-echo "Enter MYSQL_ROOT_PASSWORD:"
-read MYSQL_ROOT_PASSWORD
-echo "Enter SECRET_KEY:"
-read SECRET_KEY
 echo "Enter TWLIGHT_OAUTH_CONSUMER_KEY:"
 read TWLIGHT_OAUTH_CONSUMER_KEY
 echo "Enter TWLIGHT_OAUTH_CONSUMER_SECRET:"
 read TWLIGHT_OAUTH_CONSUMER_SECRET
 echo "Enter TWLIGHT_EZPROXY_SECRET:"
 read TWLIGHT_EZPROXY_SECRET
-echo "Enter stack environment (eg. override \| staging \| production):"
+echo "Enter stack environment (eg. override | staging | production):"
 read TWLIGHT_STACK_ENV
-echo "Enter MATOMO_SITEID:"
-read MATOMO_SITEID
-echo "Enter MATOMO_AUTH_TOKEN:"
-read MATOMO_AUTH_TOKEN
 
+# Setup DKIM
+ssh-keygen -t rsa -b 1024 -m PEM -P "" -f dkim
+ssh-keygen -f dkim.pub -m PKCS8 -e > dkim.pub.pkcs8
+DKIM_PRIVATE_KEY=$(cat dkim)
+DKIM_DNS_TXT=$(tail -n +2 dkim.pub.pkcs8 | head -n -1 | tr -d '\n')
+rm dkim dkim.pub dkim.pub.pkcs8
+
+# Generate some secrets
+DJANGO_DB_PASSWORD=$(openssl rand -hex 30)
+MYSQL_ROOT_PASSWORD=$(openssl rand -hex 30)
+SECRET_KEY=$(openssl rand -hex 50)
+
+# Deploy TWLight
 chown -R twlight:twlight /srv/TWLight
-
 read -r -d '' TWLIGHT <<- EOF
-
+echo "Initialising swarm"
 docker swarm init
-# drop any existing services
-docker service ls -q | xargs docker service rm
-# drop any existing secrets
-docker secret ls -q | xargs docker secret rm
+echo "Deleting any existing services"
+docker service ls -q | xargs docker service rm ||:
+echo "Deleting any existing secrets"
+docker secret ls -q | xargs docker secret rm ||:
 
 cd /srv/TWLight
+
+echo "Creating secrets"
 printf "${DJANGO_DB_NAME}" | docker secret create DJANGO_DB_NAME -
 printf "${DJANGO_DB_USER}" | docker secret create DJANGO_DB_USER -
 printf "${DJANGO_DB_PASSWORD}" | docker secret create DJANGO_DB_PASSWORD -
-printf "${DKIM_PRIVATE_KEY}" | docker secret create DKIM_PRIVATE_KEY -
+printf "'''${DKIM_PRIVATE_KEY}'''" | docker secret create DKIM_PRIVATE_KEY -
 printf "${MYSQL_ROOT_PASSWORD}" | docker secret create MYSQL_ROOT_PASSWORD -
 printf "${SECRET_KEY}" | docker secret create SECRET_KEY -
 printf "${TWLIGHT_OAUTH_CONSUMER_KEY}" | docker secret create TWLIGHT_OAUTH_CONSUMER_KEY -
 printf "${TWLIGHT_OAUTH_CONSUMER_SECRET}" | docker secret create TWLIGHT_OAUTH_CONSUMER_SECRET -
 printf "${TWLIGHT_EZPROXY_SECRET}" | docker secret create TWLIGHT_EZPROXY_SECRET -
-printf "${MATOMO_SITEID}" | docker secret create MATOMO_SITEID -
-printf "${MATOMO_AUTH_TOKEN}" | docker secret create MATOMO_AUTH_TOKEN -
-
+echo "Deploying stack"
 docker stack deploy -c "docker-compose.yml" -c "docker-compose.${TWLIGHT_STACK_ENV}.yml" "${TWLIGHT_STACK_ENV}"
-
 echo "Setting up crontab. *WARNING* This will create duplicate jobs if run repeatedly."
 (crontab -l 2>/dev/null; echo "# Run django_cron tasks.") | crontab -
 (crontab -l 2>/dev/null; echo '*/5 * * * *  docker exec -t \$(docker ps -q -f name=${TWLIGHT_STACK_ENV}_twlight) /app/bin/twlight_docker_entrypoint.sh python manage.py runcrons') | crontab -
@@ -93,4 +102,9 @@ echo "Setting up crontab. *WARNING* This will create duplicate jobs if run repea
 (crontab -l 2>/dev/null; echo "# Reclaim disk space previously used by docker.") | crontab -
 (crontab -l 2>/dev/null; echo '*/30 * * * * docker system prune -a -f; docker volume rm \$(docker volume ls -qf dangling=true)') | crontab -
 EOF
+
 sudo su --login twlight /usr/bin/env bash -c "${TWLIGHT}"
+
+echo "NOTE: YOU MUST CREATE THE FOLLOWING DNS ENTRY FOR EMAIL TO WORK:"
+echo " name                               | type | record "
+echo " ${TWLIGHT_STACK_ENV}._domainkey.twl.wmflabs.org. | TXT | \"v=DKIM1;t=s;p=${DKIM_DNS_TXT}\" "
