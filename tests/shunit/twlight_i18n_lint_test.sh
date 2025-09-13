1b088f8193bb90517cc6ded336bd661e0de590a8
jsnshrmn <2986893+jsnshrmn@users.noreply.github.com>
2025-09-12 22:28:49 -0500

    build/deploy: drop matomo-related things
    - Delete dangling cruft from a long-abandoned matomo/syslog implementation
    - Fix Dockerfile style issues
    - Add 1y expiration label to images to limit registry storage usage
    - Fix small bug for i18n lint messages on assertion failure
    
    Bug: T402054

--- tests/shunit/twlight_i18n_lint_test.sh
+++ tests/shunit/twlight_i18n_lint_test.sh
@@ -4,25 +4,25 @@ shopt -s expand_aliases
 
 testBadPyNewlines() {
     prefix=${TWLIGHT_HOME}/tests/shunit/data/bad_i18n_newline_
-    for i in ${prefix}*.py
+    for file in ${prefix}*.py
     do
-        assertFalse "${file} should cause an error." "${TWLIGHT_HOME}/bin/twlight_i18n_lint.pl ${i}"
+        assertFalse "${file} should cause an error." "${TWLIGHT_HOME}/bin/twlight_i18n_lint.pl ${file}"
     done
 }
 
 testGoodPy() {
     prefix=${TWLIGHT_HOME}/tests/shunit/data/good_i18n_
-    for i in ${prefix}*.py
+    for file in ${prefix}*.py
     do
-        assertTrue "${file} should not cause an error." "${TWLIGHT_HOME}/bin/twlight_i18n_lint.pl ${i}" ||:
+        assertTrue "${file} should not cause an error." "${TWLIGHT_HOME}/bin/twlight_i18n_lint.pl ${file}" ||:
     done
 }
 
 testBadPyComments() {
     prefix=${TWLIGHT_HOME}/tests/shunit/data/bad_i18n_comment_
-    for i in ${prefix}*.py
+    for file in ${prefix}*.py
     do
-        assertFalse "${file} should cause an error." "${TWLIGHT_HOME}/bin/twlight_i18n_lint.pl ${i}"
+        assertFalse "${file} should cause an error." "${TWLIGHT_HOME}/bin/twlight_i18n_lint.pl ${file}"
     done
 }
 
