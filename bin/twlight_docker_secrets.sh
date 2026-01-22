f638c344fbcd5887815e9ed655173fc42a39f084
Jason Sherman <jsnshrmn@users.noreply.github.com>
2026-01-22 12:47:27 -0600

    HOTFIX: ops: add support for MW API EMAIL USER/PW
    Bug: T409420

--- bin/twlight_docker_secrets.sh
+++ bin/twlight_docker_secrets.sh
@@ -3,58 +3,58 @@
 if  [ ! -n "${DJANGO_DB_NAME+isset}" ]
 then
     DJANGO_DB_NAME=$(cat /run/secrets/DJANGO_DB_NAME 2>/dev/null)
-	export DJANGO_DB_NAME
+    export DJANGO_DB_NAME
 fi
 
 if  [ ! -n "${DJANGO_DB_USER+isset}" ]
 then
     DJANGO_DB_USER=$(cat /run/secrets/DJANGO_DB_USER 2>/dev/null)
-	export DJANGO_DB_USER
+    export DJANGO_DB_USER
 fi
 
 if  [ ! -n "${DJANGO_DB_PASSWORD+isset}" ]
 then
     DJANGO_DB_PASSWORD=$(cat /run/secrets/DJANGO_DB_PASSWORD 2>/dev/null)
-	export DJANGO_DB_PASSWORD
+    export DJANGO_DB_PASSWORD
 fi
 
 if  [ ! -n "${DKIM_PRIVATE_KEY+isset}" ]
 then
     DKIM_PRIVATE_KEY=$(cat /run/secrets/DKIM_PRIVATE_KEY 2>/dev/null)
-	export DKIM_PRIVATE_KEY
+    export DKIM_PRIVATE_KEY
 fi
 
 if  [ ! -n "${SECRET_KEY+isset}" ]
 then
     SECRET_KEY=$(cat /run/secrets/SECRET_KEY 2>/dev/null)
-	export SECRET_KEY
+    export SECRET_KEY
 fi
 
 if  [ ! -n "${TWLIGHT_OAUTH_CONSUMER_KEY+isset}" ]
 then
     TWLIGHT_OAUTH_CONSUMER_KEY=$(cat /run/secrets/TWLIGHT_OAUTH_CONSUMER_KEY 2>/dev/null)
-	export TWLIGHT_OAUTH_CONSUMER_KEY
+    export TWLIGHT_OAUTH_CONSUMER_KEY
 fi
 
 if  [ ! -n "${TWLIGHT_OAUTH_CONSUMER_SECRET+isset}" ]
 then
     TWLIGHT_OAUTH_CONSUMER_SECRET=$(cat /run/secrets/TWLIGHT_OAUTH_CONSUMER_SECRET 2>/dev/null)
-	export TWLIGHT_OAUTH_CONSUMER_SECRET
+    export TWLIGHT_OAUTH_CONSUMER_SECRET
 fi
 
 if  [ ! -n "${TWLIGHT_EZPROXY_SECRET+isset}" ]
 then
     TWLIGHT_EZPROXY_SECRET=$(cat /run/secrets/TWLIGHT_EZPROXY_SECRET 2>/dev/null)
-	export TWLIGHT_EZPROXY_SECRET
+    export TWLIGHT_EZPROXY_SECRET
 fi
 
 if  [ ! -n "${MW_API_EMAIL_USER+isset}" ]
 then
-	MW_API_EMAIL_USER=$(cat /run/secrets/MW_API_EMAIL_USER 2>/dev/null)
-	export MW_API_EMAIL_USER
+    MW_API_EMAIL_USER=$(cat /run/secrets/MW_API_EMAIL_USER 2>/dev/null)
+    export MW_API_EMAIL_USER
 fi
 if  [ ! -n "${MW_API_EMAIL_PASSWORD+isset}" ]
 then
     MW_API_EMAIL_PASSWORD=$(cat /run/secrets/MW_API_EMAIL_PASSWORD 2>/dev/null)
-	export MW_API_EMAIL_PASSWORD
+    export MW_API_EMAIL_PASSWORD
 fi
